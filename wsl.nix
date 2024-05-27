22b1d8243efaadb13654c0a550c15d6a60893624
Adam Dinwoodie <adam@dinwoodie.org>
2024-05-27 12:51:12 +0100

    Disable lingering on WSL
    This seems to be necessary for user systemctl commands to work!

--- wsl.nix
+++ wsl.nix
@@ -26,6 +26,16 @@
       wslu  # For wslview
     ];
 
+    # TODO Work out why having linger enabled manages to _break_ commands like
+    # `systemctl --user status`.  Probably related to
+    # https://github.com/microsoft/WSL/issues/10205 although I don't quite
+    # understand how.
+    #
+    # Ideally this would apply the configuration to all users that have
+    # isNormalUser, but I can't work out how to do that without infinite
+    # recursion :(
+    users.users.adam.linger = lib.mkForce false;
+
     nix.channels.nixos-wsl = https://github.com/nix-community/NixOS-WSL/archive/refs/heads/main.tar.gz;
   };
 }
