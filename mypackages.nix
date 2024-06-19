d18c8e1adfde823fa5d6ea629148518a6412c3b3
Adam Dinwoodie <adam@dinwoodie.org>
2024-06-19 14:00:48 +0100

    Remove unused inherit statement

--- mypackages.nix
+++ mypackages.nix
@@ -1,7 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
-let
-  inherit (pkgs) callPackage;
-in {
+{
   inherit (import ./git { inherit pkgs; }) gitMaster gitNext;
 
   inherit (import ./nix-about { inherit pkgs; }) nix-about;
