d698f40ca2b99db6c3dbdbd6f5ff5f9eabc0d5ca
Adam Dinwoodie <adam@dinwoodie.org>
2024-06-24 18:27:14 +0100

    WSL: pick the right UID
    Seems the trick for UIDs is to check whether the WSL username matches
    the Windows username, so do that.

--- nixos-platform/wsl.nix
+++ nixos-platform/wsl.nix
@@ -3,6 +3,14 @@ let
   tryWslModule = builtins.tryEval <nixos-wsl/modules>;
   hasWslModule = tryWslModule.success;
   wslModulePath = tryWslModule.value;
+
+  windowsUsername = builtins.readFile (
+    pkgs.runCommandLocal "username" {__noChroot = true;}
+    ''
+      /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -c '$env:UserName' |
+          ${pkgs.coreutils}/bin/tr -d '\r\n' >$out
+    ''
+  );
 in {
   imports = lib.optional hasWslModule wslModulePath;
 
@@ -70,9 +78,14 @@ in {
       # recursion :(
       linger = lib.mkForce false;
 
-      # TODO Work out why WSL gives warnings about user IDs without this
-      # configuration.
-      uid = lib.mkForce 1001;
+      # If the WSL username matches the host username, use UID 1000.  If not,
+      # use UID 1001.  Seems like this shouldn't be necessary, but it avoids a
+      # bunch of error messages.
+      uid = lib.mkForce (
+        if config.wsl.defaultUser == windowsUsername
+        then 1000
+        else 1001
+      );
     };
 
     nix.channels.nixos-wsl =
