e3a0578559c68775fbac18046656ba44777e896d
Adam Dinwoodie <adam@dinwoodie.org>
2024-07-03 11:59:45 +0100

    colourmail: add runtime dependencies

--- colourmail/default.nix
+++ colourmail/default.nix
@@ -1,8 +1,14 @@
 {
+  lib,
+  colorized-logs,
+  mailutils,
+  perlPackages,
   fetchFromGitHub,
   stdenvNoCC,
+  makeWrapper,
 }: let
   version = "1.0.0";
+  runtimeDeps = [perlPackages.mimeConstruct colorized-logs mailutils];
 in
   stdenvNoCC.mkDerivation {
     pname = "colourmail";
@@ -13,8 +19,11 @@ in
       rev = "v${version}";
       hash = "sha256-ruQTeCwObqNKPRyDB7FRTdvbtnaI1f1PX/93wKumoNY=";
     };
+    nativeBuildInputs = [makeWrapper];
     installPhase = ''
       mkdir -p $out/bin
       cp colourmail $out/bin/
+      wrapProgram $out/bin/colourmail \
+          --prefix PATH : ${lib.makeBinPath runtimeDeps}
     '';
   }
