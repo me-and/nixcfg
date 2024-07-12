551237edd0c5bb2d5968110402d0efaf3d4aaceb
Adam Dinwoodie <adam@dinwoodie.org>
2024-07-12 11:31:17 +0100

    Home: add nixcfg to list of Homeshick repos

--- home.nix
+++ home.nix
@@ -107,6 +107,7 @@ in {
     enable = true;
     repos = [
       (doLink "https://github.com/me-and/castle")
+      (dontLink "https://github.com/me-and/nixcfg")
       (dontLink "https://github.com/mileszs/ack.vim")
       (dontLink "https://github.com/me-and/asmodeus")
       (dontLink "https://github.com/magicmonty/bash-git-prompt")
