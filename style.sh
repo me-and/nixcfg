9adf0cb8e647fd7329fddd9dd5dfcf5a1037d344
Adam Dinwoodie <adam@dinwoodie.org>
2024-07-16 12:59:54 +0100

    fixup! Home: add my home-manager repl patch

--- style.sh
+++ style.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 git ls-files -z |
 	grep -z '\.nix$' |
-	grep -vz 'overlays/home-manager/home-manager.nix' |
+	grep -vxz 'overlays/home-manager/home-manager.nix' |
 	xargs -0 alejandra -c
