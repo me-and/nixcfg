9222e8690075ac37c2c2ae0cecd51ee9628073ec
Adam Dinwoodie <adam@dinwoodie.org>
2024-07-17 10:08:41 +0100

    nix-about: allow specifying alternate nixpkgs

--- pkgs/nix-about/package.nix
+++ pkgs/nix-about/package.nix
@@ -5,8 +5,36 @@
 writeShellApplication {
   name = "nix-about";
   text = ''
+    set_nixpkgs_args () {
+        nixpkgs_args+=(--arg pkgs "import ($1) {}")
+    }
+
+    packages=()
+    nixpkgs_args=()
+
+    while (( $# > 0 )); do
+        case "$1" in
+        -p|--nixpkgs)
+            set_nixpkgs_args "$2"
+            shift 2
+            ;;
+        -p*)
+            set_nixpkgs_args "''${1#-p}"
+            shift
+            ;;
+        --nixpkgs=*)
+            set_nixpkgs_args "''${1#--nixpkgs=}"
+            shift
+            ;;
+        *)
+            packages+=("$1")
+            shift
+            ;;
+        esac
+    done
+
     first=Yes
-    for arg; do
+    for p in "''${packages[@]}"; do
         if [[ "$first" ]]; then
             first=
         else
@@ -16,7 +44,8 @@ writeShellApplication {
             --extra-experimental-features nix-command \
             eval \
             --read-only \
-            --argstr pkgname "$arg" \
+            --argstr pkgname "$p" \
+            "''${nixpkgs_args[@]}" \
             --file ${./about.nix} \
             --raw \
             output
