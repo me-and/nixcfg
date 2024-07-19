afa6e3cf3fd55130dab5437993a1ccd33214e471
Adam Dinwoodie <adam@dinwoodie.org>
2024-07-19 21:30:14 +0100

    pkgs: default to using local overlays

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,11 +1,18 @@
-{
-  pkgs ? import <nixpkgs> {},
-  lib ? pkgs.lib,
-  callPackage ? pkgs.callPackage,
-}: let
-  inherit (lib.attrsets) filterAttrs mapAttrs;
-
-  possiblePackageFiles = mapAttrs (name: _: ./. + "/${name}/package.nix") (builtins.readDir ./.);
-  packageFiles = filterAttrs (_: value: builtins.pathExists value) possiblePackageFiles;
+let
+  overlayDir = ../overlays;
+  overlayFiles =
+    lib.attrsets.mapAttrsToList
+    (name: value: lib.apth.append overlayDir name)
+    (builtins.readDir overlayDir);
 in
-  mapAttrs (name: value: callPackage value {}) packageFiles
+  {
+    pkgs ? import <nixpkgs> {overlays = map import overlayFiles;},
+    lib ? pkgs.lib,
+    callPackage ? pkgs.callPackage,
+  }: let
+    inherit (lib.attrsets) filterAttrs mapAttrs;
+
+    possiblePackageFiles = mapAttrs (name: _: ./. + "/${name}/package.nix") (builtins.readDir ./.);
+    packageFiles = filterAttrs (_: value: builtins.pathExists value) possiblePackageFiles;
+  in
+    mapAttrs (name: value: callPackage value {}) packageFiles
