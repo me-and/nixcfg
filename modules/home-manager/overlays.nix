ad6606c07c4646564a7c495a5e4d34287234d9a4
Adam Dinwoodie <adam@dinwoodie.org>
2024-07-20 20:45:23 +0100

    Home Manager overlays: don't rebuild every commit
    Clean the directory before adding it to the Nix store, so it doesn't
    change every time anything in the .git directory changes.

--- modules/home-manager/overlays.nix
+++ modules/home-manager/overlays.nix
@@ -2,7 +2,18 @@
   config,
   lib,
   ...
-}: {
+}: let
+  thisRepoCleaned = lib.sources.cleanSourceWith {
+    src = ../../.;
+    # Filter out all hidden files, including but not limited to the .git
+    # directory.
+    filter = path: type: let
+      baseName = builtins.baseNameOf path;
+    in
+      (builtins.match "\\..*" baseName) == null;
+    name = "nixcfg";
+  };
+in {
   # Copy the local directory to the Nix store, and add a reference to the
   # overlays directory within to the NIX_PATH variable.  This means invocations
   # of commands like `nix-build` will find the overlays from the environment
@@ -14,7 +25,9 @@
     type = lib.types.listOf lib.types.str;
     default = [];
   };
-  config.nix.nixPath = ["nixpkgs-overlays=${../../.}/overlays"];
+
+  config.nix.nixPath = ["nixpkgs-overlays=${thisRepoCleaned}/overlays"];
+
   config.home.sessionVariables = lib.mkIf (config.nix.nixPath != []) {
     NIX_PATH = "${lib.strings.concatStringsSep ":" config.nix.nixPath}\${NIX_PATH:+:$NIX_PATH}";
   };
