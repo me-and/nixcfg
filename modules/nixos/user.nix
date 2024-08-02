f89490281d266eb6c1e71a585213652d6b1c4672
Adam Dinwoodie <adam@dinwoodie.org>
2024-08-02 11:21:46 +0100

    NixOS: simpler username config
    Add a `users.me` configuration option that has my username, so it's
    easier to pass around between modules.

--- modules/nixos/user.nix
+++ modules/nixos/user.nix
@@ -7,30 +7,29 @@
   # necessary, but in practice a bunch of things need extra work without this
   # (e.g. the UIDs get confused, and you can't launch GUI applications as a
   # result).
-  #
-  # TODO Remove duplication with modules/nixos/pd/default.nix
   username =
     if config.system.isWsl
     then config.wsl.defaultUser
     else "adam";
 
-  passwordConfig =
-    if builtins.pathExists ../../secrets/adam
-    then {hashedPasswordFile = builtins.toString ../../secrets/adam;}
-    else if builtins.pathExists ../../passwords/adam
-    then {hashedPasswordFile = builtins.toString ../../passwords/adam;}
-    else {};
+  cfg = config.users.users;
 in {
+  options.users.me = lib.mkOption {
+    type = lib.types.str;
+    description = "My username";
+    default = username;
+  };
+
   # Always want fixed users.
   users.mutableUsers = false;
 
   # Set up my user account.
-  users.users."${username}" =
+  users.users."${cfg.me}" =
     {
       isNormalUser = true;
       description = "Adam Dinwoodie";
       extraGroups = ["wheel"];
       linger = true;
-    }
-    // passwordConfig;
+      hashedPasswordFile = builtins.toString ../../secrets/adam;
+    };
 }
