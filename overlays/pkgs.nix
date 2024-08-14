aa85664e0ebeb24ae2e972371a387511fc98553d
Adam Dinwoodie <adam@dinwoodie.org>
2024-08-14 13:43:33 +0100

    Simplify pkgs and overlays
    In particular, remove the reliance on importing <nixpkgs/lib> from the
    environment, by (a) using builtin Nix functions and (b) calling
    functions from prev.lib in the overlay.

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -1,11 +1,22 @@
 # This file based heavily on <nixpkgs/pkgs/top-level/by-name-overlay.nix>
-let
-  lib = import <nixpkgs/lib>;
-
-  inherit (lib.attrsets) filterAttrs mapAttrs;
+#
+# This file needs to work without importing the pkgs directly as a whole, since
+# pkgs/default.nix depends on overlays.  It can also only use `lib` from `prev`
+# for computing the names of the things being overridden, as otherwise Nix
+# can't tell if one of the values being overridden in `final` is `lib` itself.
+final: prev: let
+  inherit (prev.lib.attrsets) mapAttrs filterAttrs;
+  inherit (prev.lib) path;
+  inherit (builtins) readDir pathExists;
 
   pkgDir = ../pkgs;
-  possiblePackageFiles = mapAttrs (name: _: pkgDir + "/${name}/package.nix") (builtins.readDir pkgDir);
-  packageFiles = filterAttrs (_: value: builtins.pathExists value) possiblePackageFiles;
+  possiblePackageFiles =
+    mapAttrs
+    (name: _: path.append pkgDir "${name}/package.nix")
+    (readDir pkgDir);
+  packageFiles =
+    filterAttrs
+    (_: value: pathExists value)
+    possiblePackageFiles;
 in
-  final: prev: mapAttrs (name: value: final.callPackage value {}) packageFiles
+  mapAttrs (name: value: final.callPackage value {}) packageFiles
