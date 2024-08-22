bfd30f35ace7052767facb9532340be644af47be
Adam Dinwoodie <adam@dinwoodie.org>
2024-08-22 08:38:31 +0100

    tmux-taskloop: sort layouts out
    Now with working dynamic layouts!  Or at least layout, singular, with a
    message when the screen is to small and an obvious place to put new
    layouts.

--- pkgs/tmux-taskloop/package.nix
+++ pkgs/tmux-taskloop/package.nix
@@ -6,62 +6,63 @@
   writeTextFile,
   writeCheckedShellApplication,
 }: let
-  tmuxCommands = [
-    # Start the session.  We'll have a single session called "taskloop" with a
-    # single window with the same name.
-    #
-    # Lots of examples online use `-d` here to avoit attaching to the session
-    # straight away, and instead attach to the session once it is set up.
-    # Don't do that here, because that prevents the session knowing the correct
-    # size for its windows, and passing that information through seems
-    # remarkably complicated.
-    #
-    # TODO: Ability to have multiple sessions in parallel.
-    "new-session -s taskloop -n taskloop -e TMUX_TASKLOOP=Yes"
+  tmuxStartupConf = writeTextFile {
+    name = "tmux-taskloop.conf";
+    text = ''
+      # Start the prompts we want to keep around.
+      new-session -s taskloop -n taskloop -e TMUX_TASKLOOP=Yes
+      new-window -d -n taskloop-next ${bashInteractive}/bin/bash -c 'read; exec ${taskloop}/bin/taskloop -ns'
+      new-window -d -n taskloop-waitingfor ${bashInteractive}/bin/bash -c 'read; exec ${taskloop}/bin/taskloop -ns waitingfor'
 
-    # If any pane -- normally the Bash prompt pane -- exits, terminate the
-    # entire session.
-    "set-hook -w pane-exited kill-session"
+      # Disable the status line and prefix keys, so any inner/outer tmux window
+      # works normally.
+      set-option status off
+      set-option prefix None
+      set-option prefix2 None
 
-    # Create a new pane to the left of the first pane, and launch taskloop in
-    # that new pane.
-    "split-window -b -h ${bashInteractive}/bin/bash -c 'read; exec ${taskloop}/bin/taskloop -n'"
+      # Set up the window layout for the current window size.
+      source-file ${tmuxResizeConf}
 
-    # Create a new pane below the previous pane, with the waitingfor taskloop.
-    "split-window -v ${bashInteractive}/bin/bash -c 'read; exec ${taskloop}/bin/taskloop -n waitingfor'"
+      # Set hooks to exit when the command prompt exits, and to resize when the
+      # window resizes.
+      set-hook -w pane-exited kill-session
+      set-hook -w window-resized {
+          break-pane -d -s %1 -n taskloop-next
+          break-pane -d -s %2 -n taskloop-waitingfor
+          source-file ${tmuxResizeConf}
+      }
+    '';
+  };
 
-    # Set the panes to resize automatically if the window is resized.  This
-    # also seems to be triggered at start of day, which is handy, as it means
-    # the config is set automatically!
-    #
-    # Set the command window to always be 80 columns wide, and set the two
-    # taskloop windows to be 50% of their column.
-    "set-hook -w window-resized { resize-pane -t %0 -x 80 ; resize-pane -t %1 -y 50% }"
+  # This needs to be a separate file, as otherwise tmux seems to parse the
+  # `%if` conditionals when it first reads the config file, which means that
+  # calling a hook after a resize won't be very useful, as the behaviour will
+  # be fixed by the state of the window when the hook was configured, rather
+  # than when the hook is run.
+  tmuxResizeConf = writeTextFile {
+    name = "tmux-taskloop-resize.conf";
+    text = ''
+      %if "#{e|>:#{window_width},160}"
+          # Pull in the panes from the taskloop-next and taskloop-waitingfor
+          # windows.  Arrange them one on top of another on the left, with the
+          # prompt window on the right and 80 columns wide.
+          join-pane -b -h -s =taskloop:taskloop-next -t %0
+          join-pane -v -s =taskloop:taskloop-waitingfor -t %1
+          resize-pane -t %0 -x 80
+          select-pane -t %0
 
-    # Return to the first pane.
-    "select-pane -t %0"
-
-    # Disable the prefix keys, since now we're set up I won't need to control
-    # tmux from within the session, and would rather any tmux prefix controls
-    # are passed through.
-    "set-option prefix None"
-    "set-option prefix2 None"
-
-    # Disable the status line, because I also don't need that now I have the
-    # tmux session started.  Handily, this also triggers the "window-resized"
-    # hook to set the panes to the right size.
-    "set-option status off"
-
-    # Trigger the two taskloop panes to actually load, now their size is fixed.
-    "send-keys -t %1 C-m"
-    "send-keys -t %2 C-m"
-  ];
-
-  # Use " ; " to join the Tmux commands, since that will mean if an earlier
-  # command fails, later commands won't run.
-  tmuxCommandFile = writeTextFile {
-    name = "tmux-taskloop-conf";
-    text = lib.strings.concatStringsSep " ; " tmuxCommands;
+          # Send Return to the two report windows, to trigger them to redraw or
+          # run the initial report.
+          send-keys -t %1 C-m
+          send-keys -t %2 C-m
+      %else
+          # Turn on the status line so the message is visible, then show a
+          # warning.
+          set-option status on
+          display-message -d0 "Don't know how to handle a #{window_width}x#{window_height} window!"
+          set-option status off
+      %endif
+    '';
   };
 in
   writeCheckedShellApplication {
@@ -69,6 +70,6 @@ in
     text = ''
       exec ${tmux}/bin/tmux \
           start-server \; \
-          source-file ${tmuxCommandFile}
+          source-file ${tmuxStartupConf}
     '';
   }
