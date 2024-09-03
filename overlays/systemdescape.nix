5ce733cad65c421a106193194006c74f18af5613
Adam Dinwoodie <adam@dinwoodie.org>
2024-09-03 10:02:34 +0100

    escapeSystemdString: new pkgs function

--- overlays/systemdescape.nix
+++ overlays/systemdescape.nix
@@ -1,6 +1,11 @@
 final: prev: let
   inherit (final) lib runCommandLocal systemd;
 in {
+  escapeSystemdString = str:
+    lib.strings.fileContents (
+      runCommandLocal "escape" {}
+      "${systemd}/bin/systemd-escape ${lib.strings.escapeShellArg str} >$out"
+    );
   escapeSystemdPath = str:
     lib.strings.fileContents (
       runCommandLocal "escape" {}
