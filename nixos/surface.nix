3ab318df01de6d46a70a79f83e6bf3a1864b21f2
Adam Dinwoodie <adam@dinwoodie.org>
2024-09-05 18:17:09 +0100

    NixOS: Add Surface config

---
+++ nixos/surface.nix
@@ -0,0 +1,14 @@
+{
+  imports = [./common];
+
+  system.stateVersion = "24.05";
+  system.isWsl = true;
+  networking.hostName = "surface";
+  networking.domain = "dinwoodie.org";
+
+  services.postfix = {
+    enable = true;
+    relayHost = "smtp.tastycake.net";
+    relayPort = 587;
+  };
+}
