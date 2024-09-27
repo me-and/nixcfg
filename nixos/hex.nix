8e14b44b12790f231458a56d38ddf5a57080bf4a
Adam Dinwoodie <adam@dinwoodie.org>
2024-09-27 22:13:22 +0100

    NixOS/Hex: add new system config

---
+++ nixos/hex.nix
@@ -0,0 +1,28 @@
+{
+  imports = [
+    ./hex-hardware.nix
+    ./common
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "hex";
+  networking.domain = "dinwoodie.org";
+
+  networking.networkmanager.enable = true;
+
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.postfix = {
+    enable = true;
+    relayHost = "smtp.tastycake.net";
+    relayPort = 587;
+  };
+
+  nix.settings.substituters = ["http://192.168.1.131"];
+
+  system.stateVersion = "24.05";
+}
