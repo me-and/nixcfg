1930fc178efeabafd5e71e6ed263e02169de535d
Adam Dinwoodie <adam@dinwoodie.org>
2024-10-09 10:49:22 +0100

    CI: support for specifying GitHub CI buildability
    Add a githubBuildPlatforms meta attribute to my packages that specifies
    whether any given package can be build using my GitHub actions, and
    don't bother trying to build things on GitHub that won't build there.

---
+++ lib/github-testable-packages.nix
@@ -0,0 +1,32 @@
+# Filter the list of packages to only provide the names of ones that can be
+# built as part of GitHub actions.  Assume a package can be built unless (a)
+# meta.githubBuildPlatforms is defined and doesn't include the build platform,
+# or (b) meta.githubBuildPlatforms is not defined but meta.platforms is and
+# doesn't include the build platform.
+#
+# This emulates the meta.hydraPlatforms interface used by the Nixpkgs repository.
+let
+  overlayDir = ../overlays;
+  overlayFileNames = builtins.attrNames (builtins.readDir overlayDir);
+  overlayFiles = map (v: overlayDir + ("/" + v)) overlayFileNames;
+in
+  {
+    pkgsPath ? <nixpkgs>,
+    pkgs ? import pkgsPath {overlays = map import overlayFiles;},
+    lib ? pkgs.lib,
+  }: let
+    packages = import ../pkgs {inherit pkgsPath pkgs lib;};
+
+    buildPackage = package:
+      if package ? meta
+      then
+        if package.meta ? githubBuildPlatforms
+        then builtins.elem builtins.currentSystem package.meta.githubBuildPlatforms
+        else if package.meta ? platforms
+        then builtins.elem builtins.currentSystem package.meta.platforms
+        else true
+      else true;
+
+    buildablePackages = lib.filterAttrs (n: v: buildPackage v) packages;
+  in
+    builtins.attrNames buildablePackages
