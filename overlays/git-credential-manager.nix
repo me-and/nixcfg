6bce815016727dd7aef64502442a8133f9734ea5
Adam Dinwoodie <adam@dinwoodie.org>
2024-11-01 12:19:28 +0000

    git-credential-manager: warn when overlay is unneeded

--- overlays/git-credential-manager.nix
+++ overlays/git-credential-manager.nix
@@ -3,7 +3,10 @@ final: prev: {
   git-credential-manager = prev.git-credential-manager.overrideAttrs (prevAttrs: {
     makeWrapperArgs =
       if builtins.elem "--inherit-argv0" prevAttrs.makeWrapperArgs
-      then prevAttrs.makeWrapperArgs
+      then
+        final.lib.warn
+        "Unnecessary patch in overlay git-credential-manager.nix"
+        prevAttrs.makeWrapperArgs
       else prevAttrs.makeWrapperArgs ++ ["--inherit-argv0"];
   });
 }
