3ebbc8326de704610f331d4234cf3e9cb93cd1be
Adam Dinwoodie <adam@dinwoodie.org>
2024-12-04 10:50:38 +0000

    NixOS/24.11: handle Gnome option renames

--- nixos/common/plasma.nix
+++ nixos/common/plasma.nix
@@ -1,27 +1,45 @@
 {
   config,
   lib,
+  options,
   pkgs,
   ...
 }: let
   havePlasma = config.services.desktopManager.plasma6.enable;
 in
   lib.mkIf havePlasma {
-    environment.systemPackages = with pkgs; [
-      # I prefer the search interface in Nautilus, the Gnome file manager, to the
-      # one that comes with Plasma.  If I'm using Plasma, install Nautilus as
-      # well as the indexing tools it uses.
-      gnome.nautilus
+    environment.systemPackages = [
+      # I prefer the search interface in Nautilus, the Gnome file manager, to
+      # the one that comes with Plasma.  If I'm using Plasma, install Nautilus
+      # as well as the indexing tools it uses.
+      (pkgs.nautilus or pkgs.gnome.nautilus)
 
       # Command line clipboard access on Wayland.
       # TODO: how do I make this directly conditional on Waylang being in use,
       # rather than on Plasma implying Wayland?
-      wl-clipboard
+      pkgs.wl-clipboard
     ];
 
     # As well as enabling Nautilus, enable the indexing tools it uses.
-    services.gnome = {
-      tracker.enable = true;
-      tracker-miners.enable = true;
-    };
+    services.gnome =
+      lib.warnIf
+      ((lib ? oldestSupportedReleaseIsAtLeast)
+        && lib.oldestSupportedReleaseIsAtLeast 2411)
+      ''
+        Version handling in ${builtins.toString ./.}/plasma.nix of
+        services.gnome renames, introduced in NixOS 24.11, can be safely
+        removed.
+      ''
+      lib.mkMerge [
+        (
+          if options.services.gnome ? tinysparql
+          then {tinysparql.enable = true;}
+          else {tracker.enable = true;}
+        )
+        (
+          if options.services.gnome ? localsearch
+          then {localsearch.enable = true;}
+          else {tracker-miners.enable = true;}
+        )
+      ];
   }
