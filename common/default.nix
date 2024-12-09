7cf08cf7439b7e2f8508ef958bc58be823b3e164
Adam Dinwoodie <adam@dinwoodie.org>
2024-12-09 12:36:42 +0000

    unfree: add Kyocera drivers and a Steam package

--- common/default.nix
+++ common/default.nix
@@ -1,12 +1,14 @@
 {lib, ...}: {
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName) [
+      "cups-kyocera-3500-4500"
       "discord"
       "google-chrome"
       "netflix-icon"
       "netflix-via-google-chrome"
       "steam"
       "steam-original"
+      "steam-unwrapped"
       "steam-run"
       "zoom"
     ];
