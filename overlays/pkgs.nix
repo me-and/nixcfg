63c9af38fad5d8cc4994c9a56b9cde4667745da6
Adam Dinwoodie <adam@dinwoodie.org>
2024-12-10 14:37:13 +0000

    overlays/pkgs: warn when hiding nixpkgs package

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -18,5 +18,12 @@ final: prev: let
     filterAttrs
     (_: value: pathExists value)
     possiblePackageFiles;
+  hiddenPackageWarning = pkgName: ''
+    Package ${pkgName} exists in nixpkgs but is being overwritten by a local
+    package.
+  '';
+  fileAttrToPackage = name: value:
+    prev.lib.warnIf (builtins.hasAttr name prev) (hiddenPackageWarning name)
+    final.callPackage value {};
 in
-  mapAttrs (name: value: final.callPackage value {}) packageFiles
+  mapAttrs fileAttrToPackage packageFiles
