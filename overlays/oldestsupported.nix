8a80ae26a79c6f9be7b1a47617c9bca9e5ac33c5
Adam Dinwoodie <adam@dinwoodie.org>
2024-12-30 12:07:23 +0000

    fixup! lib.oldestSupportedReleaseIsAtLeast: add as an overlay
    style

--- overlays/oldestsupported.nix
+++ overlays/oldestsupported.nix
@@ -3,24 +3,27 @@
 # clearly true.  If neither of those apply, then throw an error; it seems
 # unlikely that's a scenario I'll ever need my config to support.
 final: prev: {
-  lib = prev.lib // {
-    oldestSupportedReleaseIsAtLeast =
-      if prev.lib ? oldestSupportedReleaseIsAtLeast
-      then
-        final.lib.warnIf (prev.lib.oldestSupportedReleaseIsAtLeast 2411)
-        ''
-          Patching in lib.oldestSupportedReleaseIsAtLeast in
-          ${toString ./.}/oldestsupported.nix is no longer necessary.
-        ''
-        prev.lib.oldestSupportedReleaseIsAtLeast
-      else
-        ver:
-        if ver >= 2405
-        then true
-        else throw ''
-          Patched lib.oldestSupportedRelease in
-          ${toString ./.}/oldestsupported.nix was never expected to handle the
-          argument ${toString ver}!
-        '';
+  lib =
+    prev.lib
+    // {
+      oldestSupportedReleaseIsAtLeast =
+        if prev.lib ? oldestSupportedReleaseIsAtLeast
+        then
+          final.lib.warnIf (prev.lib.oldestSupportedReleaseIsAtLeast 2411)
+          ''
+            Patching in lib.oldestSupportedReleaseIsAtLeast in
+            ${toString ./.}/oldestsupported.nix is no longer necessary.
+          ''
+          prev.lib.oldestSupportedReleaseIsAtLeast
+        else
+          ver:
+            if ver >= 2405
+            then true
+            else
+              throw ''
+                Patched lib.oldestSupportedRelease in
+                ${toString ./.}/oldestsupported.nix was never expected to handle the
+                argument ${toString ver}!
+              '';
     };
-  }
+}
