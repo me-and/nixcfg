833c37084d43450e87d6853ec5a98aad002acfa8
Adam Dinwoodie <adam@dinwoodie.org>
2024-12-31 12:30:30 +0000

    fixup! NixOS/taskserver: fix shellcheck error

--- nixos/common/taskserver.nix
+++ nixos/common/taskserver.nix
@@ -5,10 +5,11 @@
   ...
 }: let
   cfg = config.services.taskserver;
-in lib.mkIf cfg.enable {
-  systemd.services.taskserver-init.preStart = lib.mkForce "";
-  systemd.tmpfiles.rules = [
-    "d ${cfg.dataDir} 0770 ${cfg.user} ${cfg.group}"
-    "z ${cfg.dataDir} 0770 ${cfg.user} ${cfg.group}"
-  ];
-}
+in
+  lib.mkIf cfg.enable {
+    systemd.services.taskserver-init.preStart = lib.mkForce "";
+    systemd.tmpfiles.rules = [
+      "d ${cfg.dataDir} 0770 ${cfg.user} ${cfg.group}"
+      "z ${cfg.dataDir} 0770 ${cfg.user} ${cfg.group}"
+    ];
+  }
