181f1db820055cbb729f4cdde8acfa33953cf1e6
Adam Dinwoodie <adam@dinwoodie.org>
2025-01-02 12:51:56 +0000

    git-tip: rewrite to better use upstream builds
    Rather than having a bunch of my own custom code in the git-tip build
    process, rewrite the source package handling to use a source bundle that
    looks much more like the source bundles the Git project provides and
    that the Nixpkgs builds expect.
    
    While we're at it, rewrite the code for getting the source code to be
    able to cope with tags as well as branches.

--- pkgs/git-tip/package.nix
+++ pkgs/git-tip/package.nix
@@ -4,46 +4,71 @@
   runCommand,
   rev ? null,
   ref ? "next",
+  tag ? false,
   keepSrc ? true,
   doInstallCheck ? true,
   cacert,
   gitMinimal,
-  gnumake,
   autoconf,
+  openssl,
+  zlib,
+  curl,
+  stdenv,
 }: let
   rev' = rev;
 in let
   gitHubRepo = "gitster/git";
-  gitHubAPIInfo =
-    builtins.fetchurl
-    "https://api.github.com/repos/${gitHubRepo}/branches/${ref}";
-  rev =
-    if rev' == null
-    then (builtins.fromJSON (builtins.readFile gitHubAPIInfo)).commit.sha
-    else rev';
 
+  rev = let
+    tagAPIInfo =
+      builtins.fetchurl
+      "https://api.github.com/repos/${gitHubRepo}/tags";
+    tagInfo =
+      lib.lists.findSingle
+      (info: info.name == ref)
+      (throw "no git tag named ${ref} found")
+      (throw "multiple git tags named ${ref} found!?")
+      (builtins.fromJSON (builtins.readFile tagAPIInfo));
+    branchAPIInfo =
+      builtins.fetchurl
+      "https://api.github.com/repos/${gitHubRepo}/branches/${ref}";
+    branchInfo =
+      builtins.fromJSON (builtins.readFile branchAPIInfo);
+  in
+    if rev' != null
+    then rev'
+    else if tag
+    then tagInfo.commit.sha
+    else branchInfo.commit.sha;
+
+  # Stage one of two in preparing the source code: check out the version we've
+  # been asked for.  This is implemented directly in part to allow it to be
+  # horrendously impure by looking at what the latest release is online, and
+  # partly to allow us to keep the local .git directory *and to reference
+  # previous clones in new ones* to speed up the git clone process.
+  #
   # UUID randomly generated to make it possible to reliably find other Git
   # repos in the store, to speed up the fetch stage.
   srcUuid = "2ab053e6-dcf9-4b0d-aef2-c0d4f78bfc09";
-  src =
+  srcSrc =
     runCommand "git-src-${srcUuid}" {
       # Allow access outside the chroot jail to fetch the Git repository.
       __noChroot = true;
-      # This script runs the configure script, so give it access to all the
-      # tools that will be available at build time, plus cacert and
-      # gitMinimal to do the cloning, as well as gitMinimal to be able to use
-      # git-describe to compute a version number.
-      nativeBuildInputs = [cacert gitMinimal];
+
+      # Provide a git command to be able to do the clone.
+      nativeBuildInputs = [gitMinimal cacert];
     }
-    # This includes a reference to the commit hash even though it's not always
-    # necessary: doing this means the derivation will automatically be detected
-    # as changed when the upstream commit changes.
     ''
       set -euo pipefail
 
+      # Avoid spurious "detached head" warnings.
+      git () {
+          command git -c advice.detachedHead=false "$@"
+      }
+
       reference_args=()
-      for ref in /nix/store/*-git-src-${srcUuid}/; do
-          reference_args+=(--reference-if-able "$ref")
+      for repo in /nix/store/*-git-src-${srcUuid}/; do
+          reference_args+=(--reference-if-able "$repo")
       done
 
       git clone --branch ${lib.escapeShellArg ref} \
@@ -51,29 +76,69 @@ in let
           "''${reference_args[@]}" \
           --dissociate \
           --recurse-submodules \
-          https://github.com/${lib.escapeShellArg gitHubRepo} $out
-      cd $out
-      git switch --detach --recurse-submodules ${lib.escapeShellArg rev}
+          https://github.com/${lib.escapeShellArg gitHubRepo} "$out"
 
-      # Need to mark this as a safe directory for the version calculation
-      # magic to work.
-      HOME="$(mktemp -d)"
-      export HOME
-      git config --global safe.directory $out
-
-      # Generate GIT-VERSION-FILE for use in version calculations.
-      ${gnumake}/bin/make GIT-VERSION-FILE
+      # This step is frequently unnecessary, but it means we always have a
+      # reference to the commit hash, so the derivation will inherently change
+      # if and when the branch points to a different target.
+      git -C "$out" switch --detach --recurse-submodules ${lib.escapeShellArg rev}
     '';
+
+  # Stage two of two in preparing the source code: create a distribution
+  # tarball.  This creates a tarball more-or-less identical to the one that's
+  # created for full Git version releases, which means it can be used with the
+  # nixpkgs git packaging code that assumes the source package will contain
+  # some of the extras that are included in the release tarball and aren't in
+  # the bare repository.
+  #
+  # Building the tarball does do some actual C building, so there are a bunch
+  # of depedencies that ideally wouldn't be needed until the final, real,
+  # build.
+  src = stdenv.mkDerivation {
+    name = "git-source.tar.gz";
+    src = srcSrc;
+    outputs = ["out" "ver"] ++ lib.optional keepSrc "srcsrc";
+    nativeBuildInputs = [gitMinimal openssl zlib curl autoconf];
+    enableParallelBuilding = true;
+    dontPatch = true;
+    dontConfigure = true;
+    buildFlags = ["dist"];
+    installPhase =
+      ''
+        runHook preInstall
+
+        tarballs=(git-*.tar.gz)
+        if (( "''${#tarballs[*]}" != 1 )); then
+            echo 'Unexpected number of build tarballs!' >&2
+            exit 1
+        fi
+
+        v="''${tarballs[0]}"
+        v="''${v%.tar.gz}"
+        v="''${v#git-}"
+        echo "$v" >"$ver"
+        mv "''${tarballs[0]}" "$out"
+
+      ''
+      # If we want to keep the source in the store rather than allowing it to be
+      # garbage collected (useful as it means the next fetch doesn't need to
+      # download the entire Git source repository), add a symlink to the source
+      # in the Nix store from the output.
+      + lib.optionalString keepSrc ''
+        ln -s ${srcSrc} "$srcsrc"
+
+      ''
+      + ''
+        runHook postInstall
+      '';
+    dontFixup = true;
+  };
 
   gitOverridden = git.override {inherit doInstallCheck;};
 in
   gitOverridden.overrideAttrs (oldAttrs: rec {
     inherit src;
-    version =
-      lib.strings.removePrefix "GIT_VERSION="
-      (lib.fileContents "${src}/GIT-VERSION-FILE");
-
-    nativeBuildInputs = [autoconf] ++ oldAttrs.nativeBuildInputs;
+    version = lib.fileContents src.ver;
 
     postPatch =
       if oldAttrs.doInstallCheck
@@ -84,44 +149,14 @@ in
         ["patchShebangs t/*.sh"] ["# patchShebangs t/*.sh"]
         oldAttrs.postPatch;
 
-    # Make the configure script.  This isn't required for the release builds,
-    # because they include a pre-built configure script, but the Git branches
-    # don't include those files so they need making separately.  This based on
-    # the contents of the make command in the buildPhase in the standard
-    # builder.
-    preConfigure = let
-      # May or may not have Nixpkgs' 6bdfef9d2de2 (stdenv: generalize
-      # _accumFlagsArray to concatTo, 2024-06-09)
-      #
-      # TODO Remove this once I only care about versions that have this patch,
-      # which probably means once NixOS 24.11 is released and I've shifted all
-      # my systems over to it.
-      buildArrayCmd =
-        if lib.versionAtLeast lib.version "24.11pre-git"
-        then "concatTo flagsArray"
-        else "_accumFlagsArray";
-    in ''
-      local flagsArray=(''${enableParallelBuilding:+-j''${NIX_BUILD_CORES}} SHELL=$SHELL)
-      ${buildArrayCmd} makeFlags makeFlagsArray buildFlags buildFlagsArray
-      echoCmd 'configure build flags' "''${flagsArray[@]}"
-      make ''${makefile:+-f $makefile} "''${flagsArray[@]}" configure
-      unset flagsArray
-    '';
-    configureFlags = ["--prefix=$out"] ++ oldAttrs.configureFlags;
-
-    preInstallCheck =
-      # https://github.com/NixOS/nixpkgs/pull/314258
-      builtins.replaceStrings
-      ["disable_test t1700-split-index"] [":"]
-      oldAttrs.preInstallCheck;
 
     # If we want to keep the source in the store rather than allowing it to be
     # garbage collected (useful as it means the next fetch doesn't need to
     # download the entire Git source repository), add a symlink to the source
     # in the Nix store from the output.
     postInstall =
-      oldAttrs.postInstall
+      (oldAttrs.postInstall or "")
       + lib.optionalString keepSrc ''
-        ln -s ${src} $out/src
+        ln -s ${src.srcsrc} $out/src
       '';
   })
