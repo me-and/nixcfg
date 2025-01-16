19f5cc13810f973107f68d246a3dcbd7ea1aa123
Adam Dinwoodie <adam.dinwoodie@microsoft.com>
2025-01-16 10:50:55 +0000

    NixOS/WSL: Attempt to avoid Nix store corruption
    I've had repeated problems with Nix store corruption on WSL.  I don't
    know if using the new fsync-store-paths option will help there, but it
    seems worth a try!

--- modules/nixos/wsl.nix
+++ modules/nixos/wsl.nix
@@ -68,6 +68,18 @@ in {
       wslu # For wslview
     ];
 
+    # I've seen problems with Nix store corruption on WSL.  Hopefully this will
+    # help...
+    #
+    # Need Nix 2.25 or higher to have the fsync-store-paths option available,
+    # which also means the NixOS config can't cope with that argument yet.
+    nix.package =
+      if lib.versionAtLeast pkgs.nix.version "2.25"
+      then lib.warn "Unnecessary nix package version handling in ${toString ./.}/wsl.nix" pkgs.nix
+      else pkgs.nixVersions.nix_2_25;
+    nix.settings.fsync-metadata = true;
+    nix.extraOptions = "fsync-store-paths = true";
+
     # OS should look after the clock.  Hopefully.
     services.timesyncd.enable = false;
 
