0e2754186e6064cbda4ef4f9bc124259caa4aefc
Adam Dinwoodie <adam@dinwoodie.org>
2025-02-25 19:24:15 +0000

    Packages: handle fixes being in upstream

--- overlays/azuredatastudio.nix
+++ overlays/azuredatastudio.nix
@@ -1,10 +1,23 @@
 # https://github.com/NixOS/nixpkgs/pull/381549
-final: prev: {
-  azuredatastudio = prev.azuredatastudio.overrideAttrs (prevAttrs: {
-    nativeBuildInputs = (prevAttrs.nativeBuildInputs or []) ++ [final.wrapGAppsHook3];
-    preFixup = prevAttrs.fixupPhase;
-    # The one thing that fixupPhase-as-a-variable should do is call
-    # fixupPhase-as-a-function.
-    fixupPhase = "fixupPhase";
-  });
+final: prev: let
+  lib = final.lib;
+in {
+  azuredatastudio =
+    lib.warnIf (lib.oldestSuppportedReleaseIsAtLeast 2505)
+    ''
+      Unnecessary patching of azuredatastudio package in
+      ${./.}/azuredatastudio.nix
+    ''
+    prev.azuredatastudio.overrideAttrs (
+      prevAttrs:
+        if builtins.hasAttr "fixupPhase" prevAttrs
+        then {
+          nativeBuildInputs = (prevAttrs.nativeBuildInputs or []) ++ [final.wrapGAppsHook3];
+          preFixup = prevAttrs.fixupPhase;
+          # The one thing that fixupPhase-as-a-variable should do is call
+          # fixupPhase-as-a-function.
+          fixupPhase = "fixupPhase";
+        }
+        else {}
+    );
 }
---
+++ overlays/cardimpose.nix
@@ -0,0 +1,34 @@
+# https://github.com/NixOS/nixpkgs/pull/363750
+final: prev:
+final.lib.warnIf (final.lib.oldestSupportedReleaseIsAtLeast 2505)
+''
+  Unnecessary definition of cardimpose package in
+  ${./.}/cardimpose.nix
+''
+(
+  if builtins.hasAttr "cardimpose" prev
+  then {}
+  else {
+    cardimpose =
+      final.callPackage
+      ({
+        lib,
+        python3Packages,
+        fetchPypi,
+      }:
+        python3Packages.buildPythonApplication rec {
+          pname = "cardimpose";
+          version = "0.2.1";
+          pyproject = true;
+
+          src = fetchPypi {
+            inherit pname version;
+            hash = "sha256-7GyLTUzWd9cZ8/k+0FfzKW3H2rKZ3NHqkZkNmiQ+Tec=";
+          };
+
+          build-system = [python3Packages.setuptools];
+
+          dependencies = [python3Packages.pymupdf];
+        });
+  }
+)
