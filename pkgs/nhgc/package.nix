0fddf765184a2a159e4e6652e82d9b0c277aa8a2
Adam Dinwoodie <adam@dinwoodie.org>
2025-03-01 21:11:08 +0000

    nhgc: new package
    Based heavily on the upstream package's Nix packaging code.

---
+++ pkgs/nhgc/package.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  fetchFromGitHub,
+  nix,
+  boost,
+  python3Packages,
+}:
+python3Packages.buildPythonPackage rec {
+  pname = "nix-heuristic-gc";
+  version = "0.6.0";
+  src = fetchFromGitHub {
+    owner = "risicle";
+    repo = "nix-heuristic-gc";
+    rev = version;
+    hash = "sha256-lph+rm8qXoA6h2dJTYeuj9HJAx6PnKZSdsKBElbBUbY=";
+  };
+  NIX_SYSTEM = builtins.currentSystem; # TODO Fix for cross-compilation?
+  NIX_CFLAGS_COMPILE = ["-I${lib.getDev nix}/include/nix"];
+  buildInputs = [
+    boost
+    nix
+    python3Packages.pybind11
+    python3Packages.setuptools
+  ];
+  propagatedBuildInputs = [
+    python3Packages.humanfriendly
+    python3Packages.rustworkx
+  ];
+  checkInputs = [
+    python3Packages.pytestCheckHook
+  ];
+  preCheck = "mv nix_heuristic_gc .nix_heuristic_gc";
+}
