5c4e4ef220db91617b82dab2e95519c9558e69e5
Adam Dinwoodie <adam@dinwoodie.org>
2025-03-23 18:16:27 +0000

    squash! NixOS/Lucy: add dm-cache kernel module
    Also dm-cache-smq

--- nixos/lucy/configuration.nix
+++ nixos/lucy/configuration.nix
@@ -21,7 +21,7 @@
   };
 
   # Allow working with all LVM features I sometimes use.
-  boot.initrd.kernelModules = ["dm-snapshot" "dm-raid" "dm-mirror" "dm-cache"];
+  boot.initrd.kernelModules = ["dm-snapshot" "dm-raid" "dm-mirror" "dm-cache" "dm-cache-smq"];
 
   system.stateVersion = "24.11";
   system.isPi4 = true;
