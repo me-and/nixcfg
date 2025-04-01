c5175e57985ea99898742f412ff067ef99eb46a7
Adam Dinwoodie <adam@dinwoodie.org>
2025-04-01 17:38:39 +0100

    Home/homeshick-report: handle exit codes

--- home-manager/common/homeshick.nix
+++ home-manager/common/homeshick.nix
@@ -32,6 +32,7 @@
         ExecStart = pkgs.writeCheckedShellScript {
           name = "homeshick-report.sh";
           purePath = true;
+          bashOptions = ["nounset" "pipefail"]; # No errexit
           runtimeInputs = with pkgs; [
             coreutils
             gnused
@@ -47,6 +48,20 @@
             chronic ~/.homesick/repos/homeshick/bin/homeshick check |&
                 sed "s/^.*\\r//" |
                 ifne colourmail -s "$SUBJECT" -r "$FROM" -- "$USER"
+            rcs=("''${PIPESTATUS[@]}")
+            if (( rcs[1] != 0 )); then
+                exit "''${rcs[1]}"
+            elif (( rcs[2] != 0 )); then
+                exit "''${rcs[2]}"
+            fi
+
+            # From ~/.homesick/repos/homeshick/lib/exit_status.sh
+            declare -ir EX_AHEAD=85 EX_BEHIND=86 EX_MODIFIED=88
+            if (( rcs[0] == EX_AHEAD || rcs[0] == EX_BEHIND || rcs[0] == EX_MODIFIED )); then
+                # Expected non-zero return code from homeshick, so return 0 from this script
+                exit 0
+            fi
+            exit "''${rcs[0]}"
           '';
         };
         KillMode = "process";
