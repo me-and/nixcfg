0c88bb9430f617240fb6a3676fc0746b395fff7a
Adam Dinwoodie <adam@dinwoodie.org>
2025-04-02 22:10:41 +0100

    bash-git-prompt: new package

---
+++ pkgs/bash-git-prompt/package.nix
@@ -0,0 +1,20 @@
+{
+  fetchFromGitHub,
+  runCommandLocal,
+  python3,
+}: let
+  src = fetchFromGitHub {
+    owner = "magicmonty";
+    repo = "bash-git-prompt";
+    tag = "2.7.1";
+    hash = "sha256-FWeYzISY4+cS2xg6skfcpTXgbkBs41E/EzEb3JNdFoQ=";
+  };
+in
+  runCommandLocal "bash-git-prompt" {
+    buildInputs = [python3];
+  }
+  ''
+    cp --reflink=auto -pr ${src} $out
+    chmod -R u+w $out
+    patchShebangs $out
+  ''
