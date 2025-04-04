13a45745161aaafcc9408cc4debd36d795986c27
Adam Dinwoodie <adam@dinwoodie.org>
2025-04-04 22:49:55 +0100

    rclone: remove mostStablePackage
    Not necessary in NixOS 24.11, and seems to be broken when using flakes.

--- overlays/rclone.nix
+++ overlays/rclone.nix
@@ -1,17 +1,5 @@
 final: prev: let
   # Want the fix at https://github.com/rclone/rclone/commit/e053c8a1c03e9c4396e3588bbfc432ed4ff79814
-  rcloneBase = let
-    rcloneBase = final.lib.channels.mostStablePackageVersionAtLeast {
-      name = "rclone";
-      version = "1.67";
-      testFirst = [prev.rclone];
-      excludeOverlays = ["rclone.nix"];
-    };
-  in
-    final.lib.warnIf (final.lib.oldestSupportedReleaseIsAtLeast 2411)
-    "unnecessary version check in rclone.nix overlay"
-    rcloneBase;
-
   # https://github.com/rclone/rclone/pull/8151
   patch = final.fetchGitHubPatch {
     owner = "rclone";
@@ -20,7 +8,7 @@ final: prev: let
     hash = "sha256-CV2JGzDzOui28pPhzZUFslj+VPpsTTo/GMsBQRFtA0U=";
   };
 in {
-  rclone = rcloneBase.overrideAttrs (prevAttrs: {
+  rclone = prev.rclone.overrideAttrs (prevAttrs: {
     patches = (prevAttrs.patches or []) ++ [patch];
   });
 }
