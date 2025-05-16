20342a6089bcd95972c1dbcb1c9f7acee023157f
Adam Dinwoodie <adam.dinwoodie@alianza.com>
2025-05-16 14:13:52 +0100

    start: escape PowerShell special characters

--- pkgs/start/package.nix
+++ pkgs/start/package.nix
@@ -1,5 +1,14 @@
 {writeShellApplication}:
 writeShellApplication {
   name = "start";
-  text = "powershell.exe -Command \"start $*\"";
+  text = ''
+    command="$*"
+
+    # Powershell needs backticks and single quotes escaping, and its escape
+    # character is a backtick.
+    command="''${command//\`/\`\`}"
+    command="''${command//\'/\`\'}"
+
+    powershell.exe -Command "start $command"
+  '';
 }
