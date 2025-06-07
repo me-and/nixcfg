337fe1cba30cb209a46f38d48931540b285537b6
Adam Dinwoodie <adam@dinwoodie.org>
2025-06-07 20:19:54 +0100

    NixOS/PD: disable VPN at events

--- nixos/hex/pd.nix
+++ nixos/hex/pd.nix
@@ -1,5 +1,6 @@
-{
+{lib, ...}: {
   specialisation.pd.configuration = {
+    networking.pd.vpn = lib.mkForce false;
     networking.hosts."10.0.0.5" = [
       "profounddecisions.co.uk"
       "www.profounddecisions.co.uk"
