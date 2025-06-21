91a3fcce8d0f7afcb0439627039a6ac553565c53
Adam Dinwoodie <adam@dinwoodie.org>
2025-06-21 15:24:56 +0100

    NixOS/vim: simplify config

--- nixos/common/vim.nix
+++ nixos/common/vim.nix
@@ -3,23 +3,9 @@
   lib,
   options,
   ...
-}: let
-  commonConfig = {
-    programs.vim.defaultEditor = true;
+}: {
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
   };
-
-  uplevelConfig = lib.optionalAttrs (options.programs.vim ? enable) {
-    warnings =
-      lib.mkIf (lib.oldestSupportedReleaseIsAtLeast 2411)
-      [
-        ''
-          Version handling in ${builtins.toString ./.}/vim.nix of
-          programs.vim.enable, introduced in NixOS 24.11, can be safely
-          removed.
-        ''
-      ];
-
-    programs.vim.enable = true;
-  };
-in
-  lib.mkMerge [commonConfig uplevelConfig]
+}
