cee1373a89e99b497897c5dfbc2c01adf4c527b9
Adam Dinwoodie <adam@dinwoodie.org>
2025-08-30 11:58:24 +0100

    Home/Hex/fonts: add Atkinson Hyperlegible and enable fontconfig
    Need fontconfig for applications like LibreOffice to find the fonts I've
    installed.

--- home-manager/hex/fonts.nix
+++ home-manager/hex/fonts.nix
@@ -1,6 +1,9 @@
 {pkgs, ...}: {
+  fonts.fontconfig.enable = true;
+
   home.packages = with pkgs; [
     albertus-fonts
+    atkinson-hyperlegible-next
     cardo # Free digitisation of Bembo, aka Aldine 401
   ];
 }
