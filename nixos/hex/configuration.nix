debe21f864ece64c073bb323201dc479f430a384
Adam Dinwoodie <adam@dinwoodie.org>
2025-09-07 21:36:26 +0100

    Hex: add Preston printer drivers

--- nixos/hex/configuration.nix
+++ nixos/hex/configuration.nix
@@ -43,7 +43,8 @@
   services.printing.enable = true;
   services.printing.drivers = with pkgs; [
     cups-kyocera-3500-4500
-    hplip
+    hplip # PD GOD HP printers
+    cnijfilter2 # Colour printer in Preston
   ];
 
   programs.mosh.enable = true;
