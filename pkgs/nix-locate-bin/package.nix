02335c44cdf1b5aaaa8ba2bc2461464cf488641b
Adam Dinwoodie <adam.dinwoodie@alianza.com>
2025-09-19 10:36:35 +0100

    nix-locate-bin: handle up- and down-level versions

--- pkgs/nix-locate-bin/package.nix
+++ pkgs/nix-locate-bin/package.nix
@@ -1,7 +1,16 @@
 {
+  lib,
   writeCheckedShellApplication,
   nix-index,
 }:
+let
+  # https://github.com/NixOS/nixpkgs/issues/444284
+  nix-indexVersion = lib.strings.getVersion nix-index;
+  topLevelArg =
+    if lib.versionOlder nix-indexVersion "0.1.9"
+    then "--top-level"
+    else "";
+in
 writeCheckedShellApplication {
   name = "nix-locate-bin";
   text = ''
@@ -9,6 +18,7 @@ writeCheckedShellApplication {
         --minimal \
         --no-group \
         --type x --type s \
+        ${topLevelArg} \
         --whole-name \
         --at-root \
         "/bin/$1"
