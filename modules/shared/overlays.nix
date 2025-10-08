553b9f88f6e4c7bfe4a17fd3d95862b2fd3b2f6b
Adam Dinwoodie <adam@dinwoodie.org>
2025-10-08 22:54:07 +0100

    modules: move overlay handling

--- modules/shared/overlays.nix
+++
@@ -1,9 +0,0 @@
-{lib, ...}: let
-  overlayDir = ../../overlays;
-  overlayFiles = let
-    filenames = builtins.attrNames (builtins.readDir overlayDir);
-  in
-    map (n: lib.path.append overlayDir n) filenames;
-in {
-  nixpkgs.overlays = map import overlayFiles;
-}
