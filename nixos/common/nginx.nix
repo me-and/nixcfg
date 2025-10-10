16f25522707373440ca45ccc1eb9b6c63901d859
Adam Dinwoodie <adam@dinwoodie.org>
2025-10-10 22:25:01 +0100

    NixOS/nginx: simplify and rearrange

--- nixos/common/nginx.nix
+++ nixos/common/nginx.nix
@@ -1,7 +1,19 @@
 {
-  # I want a more informative log line.
-  services.nginx.appendHttpConfig = ''
-    log_format combined_with_host '$remote_addr - $remote_user [$time_local] $host:$server_port "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"';
-    access_log /var/log/nginx/access.log combined_with_host;
-  '';
+  services.nginx = {
+    # I want a more informative log line.
+    appendHttpConfig = ''
+      log_format combined_with_host '$remote_addr - $remote_user [$time_local] $host:$server_port "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"';
+      access_log /var/log/nginx/access.log combined_with_host;
+    '';
+
+    # Return an error for requests that aren't made to an explicitly configured
+    # virtual host.
+    virtualHosts.default = {
+      default = true;
+      # Special return code 444 causes Nginx to terminate the connection
+      # without a response.
+      locations."/".return = 444;
+      rejectSSL = true;
+    };
+  };
 }
