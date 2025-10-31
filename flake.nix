6c2e4b5fefbed1958ba6e92cccf5521ce43e8573
Adam Dinwoodie <adam@dinwoodie.org>
2025-10-31 12:46:48 +0000

    Flake: create an "everything" attribute
    This is a new attribute that can't easily be built anywhere as it
    inherently requires outputs from all architectures I'm building with.
    What it does do, however, is depend on every derivation for all
    architectures under the "checks" attribute.  If I work out the
    derivation path for this attribute, it will let me see what difference
    some change makes across all my systems, configurations and packages
    with nix-diff or similar.

--- flake.nix
+++ flake.nix
@@ -188,6 +188,13 @@
           ];
 
         formatter = pkgs.nixfmt-tree;
+
+        # A derivation that depends on all the derivations across all
+        # architectures in self.checks.  Probably not very useful to build, but
+        # comparing the before and after derivations with something like
+        # nix-diff permits seeing what difference some change makes across all
+        # my systems and packages.
+        everything = pkgs.linkFarm "everything" (mapAttrs (n: v: pkgs.linkFarm n v) self.checks);
       }
     );
 }
