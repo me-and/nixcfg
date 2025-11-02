f7b66d9774ff6bec1bc1a7ab340c11b31cb44dc5
Adam Dinwoodie <adam@dinwoodie.org>
2025-11-02 17:25:52 +0000

    taskwarrior: recurring tasks can omit due dates
    I think it should be possible to remove the due date from an instance of
    a recurring task, so add a patch that permits doing so.

--- overlays/taskwarrior/default.nix
+++ overlays/taskwarrior/default.nix
@@ -2,13 +2,15 @@
 # Report but don't prompt about repairing dependencies, because the repair will
 # never work.
 #
-# Also add a patch that's just my personal preference: include parent tasks
-# when using `task count`.
+# Also add patches that are just my personal preference: include parent tasks
+# when using `task count`, and permit removing the due date from recurring
+# tasks.
 final: prev: {
   taskwarrior2 = prev.taskwarrior2.overrideAttrs (prevAttrs: {
     patches = (prevAttrs.patches or [ ]) ++ [
       ./no-dep-repair.diff
       ./count-parents.diff
+      ./permit-undue-recurring-tasks.diff
     ];
   });
 }
