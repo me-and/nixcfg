f47fac18b62cafc1d21c117a3877ffc6faf7269f
Adam Dinwoodie <adam@dinwoodie.org>
2025-11-05 21:55:34 +0000

    pkgs: handle toil no longer existing upstream

--- default.nix
+++ default.nix
@@ -9,17 +9,8 @@
 }:
 let
   # Using unionOfDisjoint to make sure I don't override anything
-  # accidentally.  I am absolutely fine with overriding the broken "toil"
-  # package, though, so don't worry about that!
-  toilEval = builtins.tryEval pkgs.toil;
-  pkgs' =
-    if pkgs ? toil then
-      assert !toilEval.success;
-      builtins.removeAttrs pkgs [ "toil" ]
-    else
-      builtins.warn "No need to handle toil package in default.nix" pkgs;
-
-  packagesForCall = lib.attrsets.unionOfDisjoint pkgs' { inherit mylib; };
+  # accidentally.
+  packagesForCall = lib.attrsets.unionOfDisjoint pkgs { inherit mylib; };
   scope = lib.packagesFromDirectoryRecursive {
     callPackage = lib.callPackageWith packagesForCall;
     newScope = extra: lib.callPackageWith (lib.attrsets.unionOfDisjoint packagesForCall extra);
