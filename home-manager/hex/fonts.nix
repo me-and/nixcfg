b95c6976656328781caf680b121f956463774dae
Adam Dinwoodie <adam@dinwoodie.org>
2025-11-05 22:04:57 +0000

    Home/Hex: handle font package rename

--- home-manager/hex/fonts.nix
+++ home-manager/hex/fonts.nix
@@ -3,7 +3,7 @@
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
-    privatepkgs.albertus-fonts
+    privatepkgs.fonts.albertus
     atkinson-hyperlegible-next
     cardo # Free digitisation of Bembo, aka Aldine 401
   ];
