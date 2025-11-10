dd56be65b317aa2719e9d4c2d505627350c3e900
Adam Dinwoodie <adam@dinwoodie.org>
2025-11-10 21:17:32 +0000

    winapps.winapps-init: create package
    A simple helper for the first steps of setting winapps up on a new
    system.

---
+++ pkgs/winapps/winapps-init/package.nix
@@ -0,0 +1,48 @@
+{
+  inputs,
+  writeCheckedShellApplication,
+  gnused,
+  podman-compose,
+}:
+writeCheckedShellApplication {
+  name = "winapps-init";
+  runtimeInputs = [
+    gnused
+    podman-compose
+  ];
+  text = ''
+    password=
+
+    while (( $# > 0 )); do
+        case "$1" in
+        -p)
+            password="$2"
+            shift 2
+            ;;
+        -p*)
+            password="''${2#-p}"
+            shift
+            ;;
+        *)
+            echo "unexpected argument $1" >&2
+            exit 64 # EX_USAGE
+            ;;
+        esac
+    done
+
+    if [[ ! "$password" ]]; then
+        echo 'no password specified' >&2
+        exit 64 # EX_USAGE
+    fi
+
+    config_dir="$HOME"/.config/winapps
+    mkdir -p "$config_dir"
+    cp ${inputs.winapps}/compose.yaml "$config_dir"/compose.yaml
+    cp ${toString ./winapps.conf} "$config_dir"/winapps.conf
+    chmod 600 "$config_dir"/compose.yaml "$config_dir"/winapps.conf
+    sed -i 's/PASSWORD: "MyWindowsPassword"/PASSWORD: "'"$password"'"/' "$config_dir"/compose.yaml
+    sed -i 's/RDP_PASS="MyWindowsPassword"/RDP_PASS="'"$password"'"/' "$config_dir"/winapps.conf
+
+    podman-compose --file "$config_dir"/compose.yaml up
+  '';
+}
