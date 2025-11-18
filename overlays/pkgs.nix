acd529da023baac98ba68715f811f0611c3e0fdc
Adam Dinwoodie <adam@dinwoodie.org>
2025-11-18 12:48:02 +0000

    Revert "overlays/pkgs: exclude everything from mypkgs"
    This reverts commit f11d1ead6ab20d7e27eff77c11e013bea22018ca.
    
    Turns out I do rely on having everything in mypkgs, because that's how
    it gets into the flake outputs.

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -2,9 +2,9 @@
 final: prev: {
   mylib = import ../lib { inherit (final) lib; };
   mypkgs = final.lib.recurseIntoAttrs (
-    removeAttrs (import ../pkgs {
+    import ../pkgs {
       inherit inputs;
       inherit (final) lib pkgs mylib;
-    }) [ "everything" ]
+    }
   );
 }
