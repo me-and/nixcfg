f11d1ead6ab20d7e27eff77c11e013bea22018ca
Adam Dinwoodie <adam@dinwoodie.org>
2025-11-18 11:35:47 +0000

    overlays/pkgs: exclude everything from mypkgs
    It's not useful there: I'm never going to want to actually include it in
    anything accessed from an overlay.

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -1,8 +1,8 @@
 { inputs }:
 final: prev: {
   mylib = import ../lib { inherit (final) lib; };
-  mypkgs = import ../pkgs {
+  mypkgs = removeAttrs (import ../pkgs {
     inherit inputs;
     inherit (final) lib pkgs mylib;
-  };
+  }) [ "everything" ];
 }
