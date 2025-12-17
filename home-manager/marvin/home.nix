3ad2a2692a5d8d7ecd4f05148798afe868236d78
Adam Dinwoodie <adam@dinwoodie.org>
2025-12-17 13:17:44 +0000

    Home/Marvin: add packages

--- home-manager/marvin/home.nix
+++ home-manager/marvin/home.nix
@@ -1,1 +1,11 @@
-{ home.stateVersion = "25.11"; }
+{ pkgs, personalCfg, ... }:
+{
+  imports = [ personalCfg.homeModules.latex ];
+
+  home.stateVersion = "25.11";
+
+  home.packages = [
+    pkgs.mypkgs.wavtoopus
+    pkgs.quodlibet-without-gst-plugins # operon
+  ];
+}
