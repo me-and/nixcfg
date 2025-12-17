050e33d0ad3b1750e2b38b8d8405dbdf114e0a32
Adam Dinwoodie <adam@dinwoodie.org>
2025-12-17 13:24:01 +0000

    Home/Marvin: set up disk usage reports

--- home-manager/marvin/home.nix
+++ home-manager/marvin/home.nix
@@ -1,8 +1,16 @@
-{ pkgs, personalCfg, ... }:
+{
+  config,
+  pkgs,
+  personalCfg,
+  ...
+}:
 {
   imports = [ personalCfg.homeModules.latex ];
 
   home.stateVersion = "25.11";
+
+  home.file.".config/systemd/user/timers.target.wants/disk-usage-report.timer".source =
+    config.home.file.".config/systemd".source + "/user/disk-usage-report.timer";
 
   home.packages = [
     pkgs.mypkgs.wavtoopus
