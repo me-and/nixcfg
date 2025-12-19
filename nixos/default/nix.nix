8cdb874effb079466ce4aaee35e737742651b5ec
Adam Dinwoodie <adam@dinwoodie.org>
2025-12-19 09:44:58 +0000

    NixOS/nix: rejig authentication token handling
    Put the GitHub authentication token to be used by Nix in both the system
    nix.conf (via a locked down sops-nix template file) as well as in the
    daemon's environment for use by builders.

--- nixos/default/nix.nix
+++ nixos/default/nix.nix
@@ -1,14 +1,29 @@
 { config, lib, ... }:
 {
-  options.nix.daemonEnvironmentFromSops =
-    lib.mkEnableOption "getting the Nix daemon environment from SOPS."
+  options.nix.githubTokenFromSops =
+    lib.mkEnableOption "getting the GitHub authentication token for Nix from SOPS"
     // {
       default = true;
     };
 
-  config = lib.mkIf config.nix.daemonEnvironmentFromSops {
-    sops.secrets.nix-daemon-environment = { };
-    systemd.services.nix-daemon.serviceConfig.EnvironmentFile =
-      config.sops.secrets.nix-daemon-environment.path;
+  config = lib.mkIf config.nix.githubTokenFromSops {
+    sops = {
+      secrets.github-token = { };
+      templates.nix-daemon-environment.content = ''
+        NIX_GITHUB_PRIVATE_USERNAME=
+        NIX_GITHUB_PRIVATE_PASSWORD=${config.sops.placeholder.github-token}
+      '';
+      templates.auth-tokens.content = ''
+        access-tokens = github.com=${config.sops.placeholder.github-token}
+      '';
+    };
+
+    systemd.services.nix-daemon = {
+      serviceConfig.EnvironmentFile = config.sops.templates.nix-daemon-environment.path;
+      after = [ "sops-nix.service" ];
+    };
+    nix.extraOptions = ''
+      !include ${config.sops.templates.auth-tokens.path}
+    '';
   };
 }
