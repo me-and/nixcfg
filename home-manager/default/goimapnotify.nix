6bae73970ceacaa38663c74df0660a2e954d033e
Adam Dinwoodie <adam@dinwoodie.org>
2025-12-23 12:03:24 +0000

    WIP

---
+++ home-manager/default/goimapnotify.nix
@@ -0,0 +1,142 @@
+# Based on the imapnotify module in home-manager, but using goimapnotify's
+# current configuration interface.
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  notifyActionOption =
+    scenario:
+    lib.mkOption {
+      # TODO Better typing?
+      type = with lib.types; nullOr (either str package);
+      description = "Command to run ${scenario}.";
+      default = null;
+    };
+
+  cfg = config.services.goimapnotify;
+
+  accountCfgs = lib.filterAttrs (n: v: v.goimapnotify.enable) config.accounts.email.accounts;
+in
+{
+  options = {
+    services.goimapnotify = {
+      enable = lib.mkEnableOption "goimapnotify";
+
+      package = lib.mkPackageOption pkgs "goimapnotify" { };
+
+      config = lib.mkOption {
+        description = "The full configuration to pass to goimapnotify.";
+        default = {
+          configurations = lib.mapAttrsToList (n: v: v.goimapnotify.accountConfig) accountCfgs;
+        };
+      };
+
+      configFile = lib.mkOption {
+        description = "The configuration file to pass to goimapnotify.";
+        type = lib.types.path;
+        default = pkgs.writers.writeJSON "goimapnotify-config.json" cfg.config;
+      };
+    };
+
+    accounts.email.accounts = lib.mkOption {
+      type = lib.types.attrsOf (
+        lib.types.submodule (
+          { config, ... }:
+          {
+            options.goimapnotify = {
+              enable = lib.mkEnableOption "goimapnotify";
+
+              boxes = lib.mkOption {
+                type = lib.types.attrsOf (
+                  lib.types.submodule (
+                    { name, config, ... }:
+                    {
+                      options = {
+                        mailbox = lib.mkOption {
+                          type = lib.types.str;
+                          description = "The name of the mailbox/folder to synchronise";
+                          default = name;
+                        };
+
+                        onNewMail = notifyActionOption "when new mail arrives";
+                        onNewMailPost = notifyActionOption "after the onNewMail command";
+                        onChangedMail = notifyActionOption "when a flag changes on an email";
+                        onChangedMailPost = notifyActionOption "after the onChangedMail command";
+                        onDeletedMail = notifyActionOption "when mail has been deleted";
+                        onDeletedMailPost = notifyActionOption "after the onDeletedMail command";
+
+                        mailboxConfig = lib.mkOption {
+                          description = ''
+                            The full configuration for this mailbox.
+
+                            Setting this will override all the other configuration options for the mailbox.
+                          '';
+                          default = lib.filterAttrs (n: v: v != null) {
+                            inherit (config)
+                              mailbox
+                              onNewMail
+                              onNewMailPost
+                              onChangedMail
+                              onChangedMailPost
+                              onDeletedMail
+                              onDeletedMailPost
+                              ;
+                          };
+                        };
+                      };
+                    }
+                  )
+                );
+                description = throw "tbd";
+              };
+
+              accountConfig = lib.mkOption {
+                description = ''
+                  The full configuration for this email account.
+
+                  Setting this will override all other configuration options for the account.
+                '';
+                default = {
+                  inherit (config.imap) host;
+                  port =
+                    if config.imap.port != null then
+                      config.imap.port
+                    else if config.imap.tls.enable then
+                      993
+                    else
+                      143;
+                  tls = config.imap.tls.enable;
+                  tlsOptions.starttls = config.imap.tls.useStartTls;
+                  username = config.userName;
+                  passwordCmd = lib.concatMapStringsSep " " lib.escapeShellArg config.passwordCommand;
+                  boxes = lib.mapAttrsToList (n: v: v.mailboxConfig) config.goimapnotify.boxes;
+                };
+              };
+            };
+          }
+        )
+      );
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    # Based on the goimapnotify service unit file in the upstream repository.
+    systemd.user.services.goimapnotify = {
+      Service = {
+        Description = "Execute scripts on IMAP mailbox changes (new/deleted/update messages) using IDLE, golang version.";
+        StartLimitIntervalSec = "1d";
+        StartLimitBurst = 5;
+      };
+      Install.WantedBy = [ "default.target" ];
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.goimapnotify}/bin/goimapnotify -conf ${cfg.configFile}";
+        Restart = "always";
+        RestartSec = 30;
+      };
+    };
+  };
+}
--- home-manager/marvin/home.nix
+++ home-manager/marvin/home.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   pkgs,
   personalCfg,
   ...
@@ -16,6 +17,23 @@
     pkgs.mypkgs.wavtoopus
     pkgs.quodlibet-without-gst-plugins # operon
   ];
+
+  accounts.email.maildirBasePath = "${config.xdg.cacheHome}/mail";
+  programs.offlineimap.enable = true;
+
+  services.goimapnotify.enable = lib.warn "Need to set up goimapnotify" false;
+  accounts.email.accounts.main.passwordCommand = "${pkgs.coreutils}/bin/cat ${
+    config.sops.secrets."email/${config.accounts.email.accounts.main.address}/offlineimap".path
+  }";
+  accounts.email.accounts.main.goimapnotify.enable = true;
+  accounts.email.accounts.main.goimapnotify.boxes.INBOX = {
+    onNewMail = pkgs.mypkgs.writeCheckedShellScript {
+      name = "nope";
+      text = ''
+        exit 1
+      '';
+    };
+  };
 
   services.syncthing.enable = true;
 }
