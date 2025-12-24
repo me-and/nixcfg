4ee4d88406ee9a52525de86552fcccdc6c1d0a06
Adam Dinwoodie <adam@dinwoodie.org>
2025-12-24 08:51:37 +0000

    NixOS/Multivac/github-token: disable
    I don't immediately have access to the keys to set up this token, so
    just disable its use on Multivac with a warning.

--- nixos/multivac/configuration.nix
+++ nixos/multivac/configuration.nix
@@ -1,4 +1,4 @@
-{ personalCfg, ... }:
+{ lib, personalCfg, ... }:
 {
   imports = [ personalCfg.nixosModules.wsl ];
 
@@ -18,4 +18,7 @@
   # TODO Set this up, as I don't have any reason not to use it other than not
   # getting around to setting up the SSH keys.
   nix.nixBuildDotNet.substituter.enable = false;
+
+  # TODO Fix up my SOPS secrets so I don't need to keep disabling this.
+  nix.githubTokenFromSops = lib.warn "Need to set up github-token in SOPS" false;
 }
