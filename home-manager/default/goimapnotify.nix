715510d84c8a148a743efeceb6b63c1787c1a9c8
Adam Dinwoodie <adam@dinwoodie.org>
2025-12-24 10:20:07 +0000

    WIP

--- home-manager/default/goimapnotify.nix
+++ home-manager/default/goimapnotify.nix
@@ -9,12 +9,96 @@
 let
   cfg = config.services.goimapnotify;
   accountCfgs = lib.filterAttrs (n: v: v.goimapnotify.enable) config.accounts.email.accounts;
+
+  accountSubmodule =
+    { config, lib, ... }:
+    {
+      options.goimapnotify = {
+        enable = lib.mkEnableOption "goimapnotify";
+        boxes = lib.mkOption {
+          type = lib.types.attrsOf (lib.types.submodule boxSubmodule);
+          description = "Configuration for individual mailboxes.";
+        };
+
+        accountConfig = lib.mkOption {
+          description = ''
+            The full configuration for this email account.
+
+            Setting this will override all other configuration options for the account.
+          '';
+          default = {
+            inherit (config.imap) host;
+            port =
+              if config.imap.port != null then
+                config.imap.port
+              else if config.imap.tls.enable then
+                993
+              else
+                143;
+            tls = config.imap.tls.enable;
+            tlsOptions.starttls = config.imap.tls.useStartTls;
+            username = config.userName;
+            passwordCmd = lib.concatMapStringsSep " " lib.escapeShellArg config.passwordCommand;
+            boxes = lib.mapAttrsToList (n: v: v.mailboxConfig) config.goimapnotify.boxes;
+          };
+        };
+      };
+    };
+
+  notifyActionOption =
+    scenario:
+    lib.mkOption {
+      type = with lib.types; nullOr (either str package);
+      description = "Command to run ${scenario}.";
+      default = null;
+    };
+  boxSubmodule =
+    {
+      name,
+      config,
+      lib,
+      ...
+    }:
+    {
+      options = {
+        mailbox = lib.mkOption {
+          type = lib.types.str;
+          description = "The name of the mailbox/folder to synchronise";
+          default = name;
+        };
+
+        onNewMail = notifyActionOption "when new mail arrives";
+        onNewMailPost = notifyActionOption "after the onNewMail command";
+        onChangedMail = notifyActionOption "when a flag changes on an email";
+        onChangedMailPost = notifyActionOption "after the onChangedMail command";
+        onDeletedMail = notifyActionOption "when mail has been deleted";
+        onDeletedMailPost = notifyActionOption "after the onDeletedMail command";
+
+        mailboxConfig = lib.mkOption {
+          description = ''
+            The full configuration for this mailbox.
+
+            Setting this will override all the other configuration options for the mailbox.
+          '';
+          default = lib.filterAttrs (n: v: v != null) {
+            inherit (config)
+              mailbox
+              onNewMail
+              onNewMailPost
+              onChangedMail
+              onChangedMailPost
+              onDeletedMail
+              onDeletedMailPost
+              ;
+          };
+        };
+      };
+    };
 in
 {
   options = {
     services.goimapnotify = {
       enable = lib.mkEnableOption "goimapnotify";
-
       package = lib.mkPackageOption pkgs "goimapnotify" { };
 
       config = lib.mkOption {
@@ -22,6 +106,7 @@ in
         default = {
           configurations = lib.mapAttrsToList (n: v: v.goimapnotify.accountConfig) accountCfgs;
         };
+        type = with lib.types; attrsOf anything;
       };
 
       configFile = lib.mkOption {
@@ -32,92 +117,7 @@ in
     };
 
     accounts.email.accounts = lib.mkOption {
-      type = lib.types.attrsOf (
-        lib.types.submodule (
-          { config, ... }:
-          {
-            options.goimapnotify = {
-              enable = lib.mkEnableOption "goimapnotify";
-
-              boxes = lib.mkOption {
-                type = lib.types.attrsOf (
-                  lib.types.submodule (
-                    { name, config, lib, ... }:
-                    let
-                      notifyActionOption =
-                        scenario:
-                        lib.mkOption {
-                          type = with lib.types; nullOr (either str package);
-                          description = "Command to run ${scenario}.";
-                          default = null;
-                        };
-                    in
-                    {
-                      options = {
-                        mailbox = lib.mkOption {
-                          type = lib.types.str;
-                          description = "The name of the mailbox/folder to synchronise";
-                          default = name;
-                        };
-
-                        onNewMail = notifyActionOption "when new mail arrives";
-                        onNewMailPost = notifyActionOption "after the onNewMail command";
-                        onChangedMail = notifyActionOption "when a flag changes on an email";
-                        onChangedMailPost = notifyActionOption "after the onChangedMail command";
-                        onDeletedMail = notifyActionOption "when mail has been deleted";
-                        onDeletedMailPost = notifyActionOption "after the onDeletedMail command";
-
-                        mailboxConfig = lib.mkOption {
-                          description = ''
-                            The full configuration for this mailbox.
-
-                            Setting this will override all the other configuration options for the mailbox.
-                          '';
-                          default = lib.filterAttrs (n: v: v != null) {
-                            inherit (config)
-                              mailbox
-                              onNewMail
-                              onNewMailPost
-                              onChangedMail
-                              onChangedMailPost
-                              onDeletedMail
-                              onDeletedMailPost
-                              ;
-                          };
-                        };
-                      };
-                    }
-                  )
-                );
-                description = throw "tbd";
-              };
-
-              accountConfig = lib.mkOption {
-                description = ''
-                  The full configuration for this email account.
-
-                  Setting this will override all other configuration options for the account.
-                '';
-                default = {
-                  inherit (config.imap) host;
-                  port =
-                    if config.imap.port != null then
-                      config.imap.port
-                    else if config.imap.tls.enable then
-                      993
-                    else
-                      143;
-                  tls = config.imap.tls.enable;
-                  tlsOptions.starttls = config.imap.tls.useStartTls;
-                  username = config.userName;
-                  passwordCmd = lib.concatMapStringsSep " " lib.escapeShellArg config.passwordCommand;
-                  boxes = lib.mapAttrsToList (n: v: v.mailboxConfig) config.goimapnotify.boxes;
-                };
-              };
-            };
-          }
-        )
-      );
+      type = with lib.types; attrsOf (submodule accountSubmodule);
     };
   };
 
