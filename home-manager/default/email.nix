51c88d13ca7a66f1a98c172359406accb3ea2a0a
Adam Dinwoodie <adam@dinwoodie.org>
2025-12-26 18:12:12 +0000

    Home/email: add PD email config

--- home-manager/default/email.nix
+++ home-manager/default/email.nix
@@ -8,51 +8,62 @@ in
   };
 
   # Configure accounts.email.accounts.*.address in private config flake.
-  accounts.email.accounts.main = {
-    primary = true;
-    realName = "Adam Dinwoodie";
+  accounts.email.accounts = {
+    main = {
+      primary = true;
+      realName = "Adam Dinwoodie";
 
-    flavor = "gmail.com";
+      flavor = "gmail.com";
 
-    # Folders use the offlineimap format, since that's how I'm syncing them.
-    folders = {
-      inbox = "INBOX";
-      drafts = "[Gmail].Drafts";
-      sent = "[Gmail].Sent Mail";
+      # Folders use the offlineimap format, since that's how I'm syncing them.
+      folders = {
+        inbox = "INBOX";
+        drafts = "[Gmail].Drafts";
+        sent = "[Gmail].Sent Mail";
+      };
+      maildir.path = cfg.accounts.main.address;
+
+      neomutt = {
+        enable = true;
+        extraConfig = ''
+          set reverse_name = yes
+          unset reverse_realname
+          alternates '[@\.]dinwoodie\.org$' '^adam@profounddecisions\.co\.uk$' '^(adamdinwoodie|gamma3000|knightley\.nightly|sorrowfulsnail)@(gmail|googlemail)\.com$' '^adam@tastycake\.net$' '^adam\.dinwoodie@worc\.oxon\.org$'
+
+          # Don't move deleted messages to the trash; Gmail will interpret that to
+          # mean they should be deleted permanently after 30 days, where I want
+          # them to stay in my All Mail directory.
+          unset trash
+        '';
+      };
+
+      offlineimap = {
+        enable = true;
+        extraConfig = {
+          account = {
+            synclabels = true;
+            labelsheader = "X-Labels";
+          };
+          local = {
+            nametrans = "lambda f: f.replace('&', '&-')";
+            utime_from_header = true;
+          };
+          remote = {
+            maxconnections = 4;
+            remotepassfile = config.sops.secrets."email/${cfg.accounts.main.address}/offlineimap".path;
+            folderfilter = "lambda f: not f.startswith('To/') and not f.startswith('Git/') and not f.startswith('Cygwin/') and f not in ('To', 'Git', 'Cygwin', 'Retention', '[Gmail]/Important', 'Retention/Undefined', 'Retention/0')";
+            nametrans = "lambda f: f.replace('&-', '&')";
+          };
+        };
+      };
     };
-    maildir.path = cfg.accounts.main.address;
 
-    neomutt = {
-      enable = true;
-      extraConfig = ''
-        set reverse_name = yes
-        unset reverse_realname
-        alternates '[@\.]dinwoodie\.org$' '^adam@profounddecisions\.co\.uk$' '^(adamdinwoodie|gamma3000|knightley\.nightly|sorrowfulsnail)@(gmail|googlemail)\.com$' '^adam@tastycake\.net$' '^adam\.dinwoodie@worc\.oxon\.org$'
-
-        # Don't move deleted messages to the trash; Gmail will interpret that to
-        # mean they should be deleted permanently after 30 days, where I want
-        # them to stay in my All Mail directory.
-        unset trash
-      '';
-    };
-
-    offlineimap = {
-      enable = true;
-      extraConfig = {
-        account = {
-          synclabels = true;
-          labelsheader = "X-Labels";
-        };
-        local = {
-          nametrans = "lambda f: f.replace('&', '&-')";
-          utime_from_header = true;
-        };
-        remote = {
-          maxconnections = 4;
-          remotepassfile = config.sops.secrets."email/${cfg.accounts.main.address}/offlineimap".path;
-          folderfilter = "lambda f: not f.startswith('To/') and not f.startswith('Git/') and not f.startswith('Cygwin/') and f not in ('To', 'Git', 'Cygwin', 'Retention', '[Gmail]/Important', 'Retention/Undefined', 'Retention/0')";
-          nametrans = "lambda f: f.replace('&-', '&')";
-        };
+    pd = {
+      realName = "Adam Dinwoodie";
+      maildir.path = cfg.accounts.pd.address;
+      offlineimap = {
+        enable = true;
+        extraConfig.remote.remotepassfile = config.sops.secrets."email/${cfg.accounts.pd.address}".path;
       };
     };
   };
