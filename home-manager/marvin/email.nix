3388d853f342f482566e117d5974eba93c7ca6a6
Adam Dinwoodie <adam@dinwoodie.org>
2025-12-26 21:30:19 +0000

    Home/email: add password file

--- home-manager/marvin/email.nix
+++ home-manager/marvin/email.nix
@@ -8,18 +8,11 @@ in
   home.packages = [ pkgs.mypkgs.mailsync ];
 
   services.goimapnotify.enable = true;
-  accounts.email.accounts.main = {
-    passwordCommand =
-      let
-        secretPath = config.sops.secrets."email/${accountsCfg.main.address}/offlineimap".path;
-      in
-      "${pkgs.coreutils}/bin/cat ${secretPath}";
-    goimapnotify = {
-      enable = true;
-      boxes = {
-        INBOX.onNewMail = "${pkgs.mypkgs.mailsync}/bin/mailsync -i";
-        TaskWarrior.onNewMail = "${pkgs.mypkgs.mailsync}/bin/mailsync TaskWarrior";
-      };
+  accounts.email.accounts.main.goimapnotify = {
+    enable = true;
+    boxes = {
+      INBOX.onNewMail = "${pkgs.mypkgs.mailsync}/bin/mailsync -i";
+      TaskWarrior.onNewMail = "${pkgs.mypkgs.mailsync}/bin/mailsync TaskWarrior";
     };
   };
 }
