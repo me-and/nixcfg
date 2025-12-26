8375f295c06e962ca02b77ec2fb3caf171efaa5c
Adam Dinwoodie <adam@dinwoodie.org>
2025-12-26 23:37:58 +0000

    Home/Marvin/email: Move emails from PD to main

--- home-manager/marvin/email.nix
+++ home-manager/marvin/email.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   accountsCfg = config.accounts.email.accounts;
 in
@@ -8,11 +13,58 @@ in
   home.packages = [ pkgs.mypkgs.mailsync ];
 
   services.goimapnotify.enable = true;
-  accounts.email.accounts.main.goimapnotify = {
-    enable = true;
-    boxes = {
-      INBOX.onNewMail = "${pkgs.mypkgs.mailsync}/bin/mailsync -i";
-      TaskWarrior.onNewMail = "${pkgs.mypkgs.mailsync}/bin/mailsync TaskWarrior";
+  accounts.email.accounts = {
+    main.goimapnotify = {
+      enable = true;
+      boxes = {
+        INBOX.onNewMail = "${pkgs.mypkgs.mailsync}/bin/mailsync -i";
+        TaskWarrior.onNewMail = "${pkgs.mypkgs.mailsync}/bin/mailsync TaskWarrior";
+      };
+    };
+
+    pd.goimapnotify = {
+      enable = true;
+      boxes.INBOX.onNewMail = "${pkgs.mypkgs.mailsync}/bin/mailsync -e pd -i";
+    };
+  };
+
+  systemd.user = {
+    services.pd-to-main = {
+      Unit.Description = "Move PD emails to my main inbox";
+      Service = {
+        Type = "oneshot";
+        ExecStart = pkgs.mypkgs.writeCheckedShellScript {
+          name = "pd-to-main.sh";
+          runtimeInputs = [ pkgs.coreutils ];
+          text = ''
+            declare -r FROM_MAILDIR=${lib.escapeShellArg accountsCfg.pd.maildir.absPath}/INBOX
+            declare -r TO_MAILDIR=${lib.escapeShellArg accountsCfg.main.maildir.absPath}/INBOX
+
+            shopt -s nullglob
+            for f in "$FROM_MAILDIR"/new/* "$FROM_MAILDIR"/cur/*; do
+                new="$(mktemp -p "$TO_MAILDIR"/tmp)"
+                {
+                    echo 'X-Labels: To/PD'
+                    cat "$f"
+                } >"$new"
+                mv --no-clobber "$new" "$TO_MAILDIR"/new
+                rm "$f"
+            done
+
+            ${pkgs.mypkgs.mailsync}/bin/mailsync -i
+            ${pkgs.mypkgs.mailsync}/bin/mailsync -e pd -i
+          '';
+        };
+      };
+    };
+
+    paths.pd-to-main = {
+      Unit.Description = "Move PD emails to my main inbox";
+      Path.DirectoryNotEmpty = [
+        "${accountsCfg.pd.maildir.absPath}/INBOX/new"
+        "${accountsCfg.pd.maildir.absPath}/INBOX/cur"
+      ];
+      Install.WantedBy = [ "paths.target" ];
     };
   };
 }
