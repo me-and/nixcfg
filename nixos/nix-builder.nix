046ad72a0957b28b5097e63380f3cd24b2b47708
Adam Dinwoodie <adam@dinwoodie.org>
2026-01-05 12:00:41 +0000

    NixOS/nix-builder: remove nixremote user
    Looks like the nix.sshServe configuration already does everything I need
    by setting up a nix-ssh user, so remove all the manual extras I created.

--- nixos/nix-builder.nix
+++ nixos/nix-builder.nix
@@ -1,24 +1,7 @@
-let
-  permittedSshKeys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIITc1n9QPj1OdRb2v7KXdhXGHT4y2PMcr1CEXVZVqEdU AdamDinwoodie@desktop-4d6hh84-nixos"
-  ];
-in
 {
-  users.users.nixremote = {
-    isNormalUser = true;
-    description = "Remote nix build account";
-    openssh.authorizedKeys.keys = map (
-      key: "command=\"/run/current-system/sw/bin/nix-store --serve --write\" ${key}"
-    ) permittedSshKeys;
-    group = "nixremote";
-  };
-  users.groups.nixremote = { };
-
-  nix.settings.trusted-users = [ "nixremote" ];
   nix.sshServe = {
     enable = true;
     write = true;
     trusted = true;
-    keys = permittedSshKeys;
   };
 }
