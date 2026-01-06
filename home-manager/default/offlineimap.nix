2979a4a9161415106fd7e17b7abb23f71693d8bf
Adam Dinwoodie <adam@dinwoodie.org>
2026-01-06 16:40:26 +0000

    Home/offlineimap: use flock to avoid conflicts
    I've seen occasions where multiple offlineimap processes have tried to
    access the same folders.  That's a bad time, so add some flocking to
    prevent it.
    
    This means defining some of the offlineimap service units on a
    per-account basis rather than just letting systemd have a single unit
    definition, as that's the easiest way to define the paths to flock.

--- home-manager/default/offlineimap.nix
+++ home-manager/default/offlineimap.nix
@@ -1,139 +1,142 @@
 {
   config,
   lib,
+  mylib,
   pkgs,
   ...
 }:
 lib.mkIf config.programs.offlineimap.enable {
   systemd.user = {
-    services = {
-      "offlineimap-full@" = {
-        Unit = {
-          Description = "Sync of all labels for account %I";
-          After = [ "sops-nix.service" ];
-        };
-        Service = {
-          UMask = "077";
-          Type = "oneshot";
-          TimeoutStopSec = "10m";
-          ExecStart = "${config.programs.offlineimap.package}/bin/offlineimap -u basic -o -a %i";
-          SuccessExitStatus = [ "SIGTERM" ];
-          ExecStop = pkgs.mypkgs.writeCheckedShellScript {
-            name = "stop-sync-offlineimap.sh";
-            runtimeInputs = with pkgs; [
-              procps
-              coreutils
-            ];
-            text = ''
-              [[ -v MAINPID ]] || exit 0
-              pkill -P "$MAINPID" -xo offlineimap
-              rc=0
-              timeout 8m tail -f --pid "$MAINPID" /dev/null || rc="$?"
-              if (( rc == 124 )); then
-                  pkill -P "$MAINPID" -xo offlineimap
-                  pkill -P "$MAINPID" -xo offlineimap
-                  pkill -P "$MAINPID" -xo offlineimap
-                  exec timeout 1m tail -f --pid "$MAINPID"
-              else
-                  exit "$rc"
-              fi
-            '';
-          };
-        };
-      };
-
-      "offlineimap-folder@" = {
-        Unit = {
-          Description = "Sync of requested labels for account %I";
-          Requires = [ "offlineimap-folder@%i.socket" ];
-          After = [
-            "offlineimap-folder@%i.socket"
-            "sops-nix.service"
+    services =
+      let
+        accountNames = builtins.attrNames (lib.filterAttrs (n: v: v.enable) config.accounts.email.accounts);
+        stopSyncScript = pkgs.mypkgs.writeCheckedShellScript {
+          name = "stop-sync-offlineimap.sh";
+          runtimeInputs = with pkgs; [
+            procps
+            coreutils
           ];
+          text = ''
+            [[ -v MAINPID ]] || exit 0
+            pkill -P "$MAINPID" -xo offlineimap
+            rc=0
+            timeout 8m tail -f --pid "$MAINPID" /dev/null || rc="$?"
+            if (( rc == 124 )); then
+                pkill -P "$MAINPID" -xo offlineimap
+                pkill -P "$MAINPID" -xo offlineimap
+                pkill -P "$MAINPID" -xo offlineimap
+                exec timeout 1m tail -f --pid "$MAINPID"
+            else
+                exit "$rc"
+            fi
+          '';
         };
-        Service = {
-          UMask = "077";
-          Type = "oneshot";
-          StandardInput = "socket";
-          StandardOutput = "journal";
-          StandardError = "journal";
-          TimeoutStopSec = "10m";
-          SuccessExitStatus = [ "SIGTERM" ];
-          ExecStart =
-            let
-              script = pkgs.mypkgs.writeCheckedShellScript {
-                name = "sync-offlineimap-folder.sh";
-                runtimeInputs = [ config.programs.offlineimap.package ];
-                text = ''
-                  while :; do
-                      labels=()
-                      while read -rt1 label; do
-                          labels+=("$label")
-                      done
-                      (( "''${#labels[*]}" > 0 )) || exit 0
-                      IFS=,;
-                      printf "labels: %s\n" "''${labels[*]}"
-                      offlineimap -u basic -o -k mbnames:enabled=no -a "$1" -f "''${labels[*]}"
-                  done
-                '';
+        perAccountServices =
+          name:
+          let
+            maildir = config.accounts.email.accounts."${name}".maildir.absPath;
+          in
+          {
+            "offlineimap-full@${mylib.escapeSystemdString name}" = {
+              Unit = {
+                Description = "Sync of all labels for account %I";
+                After = [ "sops-nix.service" ];
               };
-            in
-            "${script} %I";
-          ExecStop = pkgs.mypkgs.writeCheckedShellScript {
-            name = "stop-sync-offlineimap-folder.sh";
-            runtimeInputs = with pkgs; [
-              procps
-              coreutils
-            ];
-            text = ''
-              [[ -v MAINPID ]] || exit 0
-              pkill -P "$MAINPID" -xo offlineimap
-              rc=0
-              timeout 8m tail -f --pid "$MAINPID" /dev/null || rc="$?"
-              if (( rc == 124 )); then
-                  pkill -P "$MAINPID" -xo offlineimap
-                  pkill -P "$MAINPID" -xo offlineimap
-                  pkill -P "$MAINPID" -xo offlineimap
-                  exec timeout 1m tail -f --pid "$MAINPID"
-              else
-                  exit "$rc"
-              fi
-            '';
+              Service = {
+                UMask = "077";
+                Type = "oneshot";
+                TimeoutStopSec = "10m";
+                ExecStart = pkgs.mypkgs.writeCheckedShellScript {
+                  name = "sync-offlineimap-${name}.sh";
+                  runtimeInputs = [
+                    pkgs.util-linux
+                    config.programs.offlineimap.package
+                  ];
+                  text = ''
+                    exec flock -Fx ${lib.escapeShellArg maildir} offlineimap -u basic -o -a ${lib.escapeShellArg name}
+                  '';
+                };
+                SuccessExitStatus = [ "SIGTERM" ];
+                ExecStop = stopSyncScript;
+              };
+            };
+
+            "offlineimap-folder@${mylib.escapeSystemdString name}" = {
+              Unit = {
+                Description = "Sync of requested labels for account %I";
+                Requires = [ "offlineimap-folder@%i.socket" ];
+                After = [
+                  "offlineimap-folder@%i.socket"
+                  "sops-nix.service"
+                ];
+              };
+              Service = {
+                UMask = "077";
+                Type = "oneshot";
+                StandardInput = "socket";
+                StandardOutput = "journal";
+                StandardError = "journal";
+                TimeoutStopSec = "10m";
+                SuccessExitStatus = [ "SIGTERM" ];
+                ExecStart = pkgs.mypkgs.writeCheckedShellScript {
+                  name = "sync-offlineimap-folder-${name}.sh";
+                  runtimeInputs = [
+                    pkgs.util-linux
+                    config.programs.offlineimap.package
+                  ];
+                  text = ''
+                    while :; do
+                        labels=()
+                        while read -rt1 label; do
+                            labels+=("$label")
+                        done
+                        (( "''${#labels[*]}" > 0 )) || exit 0
+                        IFS=,;
+                        printf "labels: %s\n" "''${labels[*]}"
+                        flock -Fx ${lib.escapeShellArg maildir} offlineimap -u basic -o -k mbnames:enabled=no -a ${lib.escapeShellArg name} -f "''${labels[*]}"
+                    done
+                  '';
+                };
+                ExecStop = stopSyncScript;
+              };
+            };
+          };
+
+        # Services where systemd instantiation gives all the granularity we need.
+        sharedServices = {
+          "offlineimap-selected-folder@" = {
+            # Instance name will be the systemd-escaped account name and
+            # systemd-escaped folder to sync, separated by `::`.
+            Unit = {
+              Description = "Sync a specific folder %i";
+              After = [ "sops-nix.service" ];
+            };
+            Service = {
+              UMask = "077";
+              Type = "oneshot";
+              ExecStart =
+                let
+                  script = pkgs.mypkgs.writeCheckedShellScript {
+                    name = "sync-offlineimap-account-folder.sh";
+                    runtimeInputs = [ pkgs.systemd ];
+                    text = ''
+                      account="$(systemd-escape -u "''${1%%::*}")"
+                      folder="$(systemd-escape -u "''${1#*::}")"
+                      folder="''${folder//&/&-}"
+                      socket_unit="$(systemd-escape --template offlineimap-folder@.socket "$account")"
+                      systemctl --user start "$socket_unit"
+                      socket_property="$(systemctl --user show -PListen "$socket_unit")"
+                      socket_path="''${socket_property% *}"
+                      echo "$folder" >"$socket_path"
+                    '';
+                  };
+                in
+                "${script} %i";
+            };
           };
         };
-      };
-
-      "offlineimap-selected-folder@" = {
-        # Instance name will be the systemd-escaped account name and
-        # systemd-escaped folder to sync, separated by `::`.
-        Unit = {
-          Description = "Sync a specific folder %i";
-          After = [ "sops-nix.service" ];
-        };
-        Service = {
-          UMask = "077";
-          Type = "oneshot";
-          ExecStart =
-            let
-              script = pkgs.mypkgs.writeCheckedShellScript {
-                name = "sync-offlineimap-account-folder.sh";
-                runtimeInputs = [ pkgs.systemd ];
-                text = ''
-                  account="$(systemd-escape -u "''${1%%::*}")"
-                  folder="$(systemd-escape -u "''${1#*::}")"
-                  folder="''${folder//&/&-}"
-                  socket_unit="$(systemd-escape --template offlineimap-folder@.socket "$account")"
-                  systemctl --user start "$socket_unit"
-                  socket_property="$(systemctl --user show -PListen "$socket_unit")"
-                  socket_path="''${socket_property% *}"
-                  echo "$folder" >"$socket_path"
-                '';
-              };
-            in
-            "${script} %i";
-        };
-      };
-    };
+      in
+      lib.mkMerge ((map perAccountServices accountNames) ++ [ sharedServices ]);
 
     sockets = {
       "offlineimap-folder@" = {
