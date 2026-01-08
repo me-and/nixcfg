a7806b7266621d22a0b309b3b9bfe4c3f8d3c7ea
Adam Dinwoodie <adam@dinwoodie.org>
2026-01-08 11:44:17 +0000

    jarvis: new box
    https://mtlynch.io/notes/nix-oracle-cloud/

---
+++ nixos/jarvis/configuration.nix
@@ -0,0 +1,106 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/v1.11.0.tar.gz"}/module.nix"
+  ];
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+    };
+    initrd.systemd.enable = true;
+  };
+
+  systemd.targets.multi-user.enable = true;
+
+  networking.hostName = "jarvis";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  users = {
+    mutableUsers = false;
+    users.adam = {
+      isNormalUser = true;
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+      ];
+      hashedPassword = "";
+    };
+    users.root.hashedPassword = "";
+  };
+
+  # Enable passwordless sudo.
+  security.sudo.extraRules = [
+    {
+      users = [ "adam" ];
+      commands = [
+        {
+          command = "ALL";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
+
+  environment.systemPackages = with pkgs; [
+    curl
+    git
+    vim
+    wget
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+    };
+  };
+
+  # Disable autologin.
+  services.getty.autologinUser = null;
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 22 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+
+  # Disable documentation for minimal install.
+  documentation.enable = false;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+  nix.gc.enable = lib.warn "want to enable garbage collection" false;
+  nix.githubTokenFromSops = lib.warn "Need to set up github-token in SOPS" false;
+  nix.nixBuildDotNet.substituter.enable = false;
+}
