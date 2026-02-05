04932ef1d64d3e7d83db96040336a4af50f0fa49
Adam Dinwoodie <adam@dinwoodie.org>
2026-02-05 16:55:39 +0000

    Home/taskwarrior-monthly: move from Lucy to Jarvis

--- home-manager/jarvis/taskwarrior/default.nix
+++ home-manager/jarvis/taskwarrior/default.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 {
+  home.file.".config/systemd/user/timers.target.wants/taskwarrior-monthly.timer".source =
+    config.home.file.".config/systemd".source + "/user/taskwarrior-monthly.timer";
+
   accounts.email.accounts = {
     taskwarrior = {
       enable = true;
--- home-manager/lucy/home.nix
+++ home-manager/lucy/home.nix
@@ -2,9 +2,6 @@
 {
   home.stateVersion = "25.11";
 
-  home.file.".config/systemd/user/timers.target.wants/taskwarrior-monthly.timer".source =
-    config.home.file.".config/systemd".source + "/user/taskwarrior-monthly.timer";
-
   programs.taskwarrior.backup.enable = true;
 
   services.syncthing.enable = true;
