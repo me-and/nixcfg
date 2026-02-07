b8b1447b24f4e406ee1a798ea445e8102a40d515
Adam Dinwoodie <adam@dinwoodie.org>
2026-02-07 19:10:08 +0000

    NixOS/marvin: update from nixos-generate-config

--- nixos/marvin/hardware-configuration.nix
+++ nixos/marvin/hardware-configuration.nix
@@ -19,7 +19,7 @@
     "thunderbolt"
     "vmd"
     "nvme"
-    "usb_storage"
+    "uas"
     "sd_mod"
   ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
@@ -40,16 +40,15 @@
     ];
   };
 
+  fileSystems."/usr/local/share/av" = {
+    device = "/dev/disk/by-uuid/06ab96b5-b34b-47e7-862d-1410dd0a5425";
+    fsType = "btrfs";
+    options = [ "subvol=@av,noexec" ];
+  };
+
   swapDevices = [
     { device = "/dev/disk/by-uuid/0fa2bff0-6803-4f5d-8b1f-b1f2de5e8e42"; }
   ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
