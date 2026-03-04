81adc326104752f00ef7d2abd016aed1504437bb
Adam Dinwoodie <adam@dinwoodie.org>
2026-03-04 13:17:03 +0000

    wip

--- demovm.sh
+++ demovm.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-rm -f -- demo.qcow2
-
 TMPDIR="$(mktemp -d)"
 export TMPDIR
 trap 'rm -rf -- "$TMPDIR"' EXIT
 
-nom build .#nixosConfigurations.demo.config.system.build.vm --out-link "$TMPDIR"/demo
+if [[ "${BASH_SOURCE[0]}" = */* ]]; then
+	flake_path="${BASH_SOURCE[0]%/*}"
+else
+	flake_path=.
+fi
+
+nom build "$flake_path"#nixosConfigurations.demo.config.system.build.vm --out-link "$TMPDIR"/demo
 
 export NIX_DISK_IMAGE="$TMPDIR"/demo.qcow2
 "$TMPDIR"/demo/bin/run-demo-vm
