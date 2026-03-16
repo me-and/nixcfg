ea846eaf0fe373cd9923a86c76043e7d378bda5b
Adam Dinwoodie <adam@dinwoodie.org>
2026-03-16 12:01:08 +0000

    NixOS/nix-index: rewrite for local generation
    I'd forgotten I was still using a version of nix-index that was pinned
    to Nixpkgs 24.05.  I've taken that as a prompt to make a bunch of
    related fixes:
    
    - Generate nix-index locally, based off the current nixpkgs.  That's
      much more memory intensive, but it means I should have a database that
      matches the version of nixpkgs I'm actually using.  And I don't need
      to worry about my Raspberry Pi's limited memory any more...
    - Remove redundant configuration that automatically gets set by
      programs.nix-index.enable.
    - Update the systemd units to check if there have been changes to the
      Nixpkgs path, and only regenerate the cache if there has been.

--- nixos/default/nix-index.nix
+++ nixos/default/nix-index.nix
@@ -1,57 +1,95 @@
-# Enable nix-index, run it automatically, and replace command-not-found with
-# it.  Use a downloaded version of the database rather than building locally,
-# because building locally is at best memory hungry, and at worst seems to take
-# my Raspberry Pi offline due to it running out of memory.
-{ pkgs, ... }:
-let
-  nix_index_dir = "/var/cache/nix-index";
-in
+{ config, pkgs, ... }:
 {
   programs.nix-index.enable = true;
-  programs.nix-index.enableBashIntegration = true;
-  programs.command-not-found.enable = false;
-  environment.variables.NIX_INDEX_DATABASE = nix_index_dir;
+  environment.variables.NIX_INDEX_DATABASE = "/var/cache/nix-index";
 
   systemd.services.nix-index = {
-    environment.NIX_INDEX_DATABASE = nix_index_dir;
+    environment = { inherit (config.environment.variables) NIX_INDEX_DATABASE; };
     wants = [ "network-online.target" ];
     after = [ "network-online.target" ];
+    path = [
+      config.programs.nix-index.package
+      pkgs.jq
+    ];
     serviceConfig.Type = "oneshot";
     serviceConfig.ExecStart = pkgs.mypkgs.writeCheckedShellScript {
       name = "update-nix-index.sh";
       text = ''
+        shopt -s nullglob
+
+        declare -ir EX_OSFILE=72
+
         target="$NIX_INDEX_DATABASE"/files
+        store_cache="$NIX_INDEX_DATABASE"/nixpkgs_path
 
-        arch="$(${pkgs.coreutils}/bin/uname -m)"
+        populate_nixpkgs_path () {
+            # This assumes using flakes, nixpkgs' path being set in the default
+            # flake registry location, and the flake registry being in the
+            # format I expect.  That's good enough for me for now, but
+            # decidedly fragile.  Although using `nix eval` to get the path
+            # seemed even more fragile!
+            nixpkgs_path="$(jq -r </etc/nix/registry.json '.flakes[] | select(.exact and .from == {id: "nixpkgs", type: "indirect"} and .to.type == "path") | .to.path')"
+            if [[ -z "$nixpkgs_path" ]]; then
+                echo 'could not find nixpkgs path in /etc/nix/registry.json' >&2
+                exit "$EX_OSFILE"
+            elif [[ ! -e "$nixpkgs_path" ]]; then
+                echo "nixpkgs path $nixpkgs_path does not exist" >&2
+                exit "$EX_OSFILE"
+            fi
+        }
 
-        # Get a temporary file name.  Fine for this to not be created
-        # securely, we just want to know it's unlikely we'll get a collision
-        # when we do the download.  curl's --no-clobber will make sure we
-        # haven't hit an obscure window condition.
-        ${pkgs.coreutils}/bin/mkdir -p "$NIX_INDEX_DATABASE"
-        tmpfile="$(${pkgs.coreutils}/bin/mktemp --dry-run --tmpdir="$NIX_INDEX_DATABASE")"
-        trap 'rm -f -- "$tmpfile"' EXIT
+        do_cache_update () {
+            mkdir -p -- "$NIX_INDEX_DATABASE"
 
-        if [[ -e "$target" ]]; then
-            curl_time_args=(--time-cond "$target")
+            populate_nixpkgs_path
+            printf '%s' "$nixpkgs_path" >"$store_cache"
+
+            # By default, nix-index starts writing the new database over the
+            # top of the existing one, which means there's a window where there
+            # is no valid database.  Avoid that by putting the new index in a
+            # temporary directory then moving it into place.
+            rm -rf -- "$NIX_INDEX_DATABASE"/tmp.nix-index.*
+            tmpdir="$(mktemp --dir "$NIX_INDEX_DATABASE"/tmp.nix-index.XXXXX)"
+
+            # nix-index likes to use carriage return in its output.  That's not
+            # very useful when logs are going to the system journal, as it
+            # results in the output mostly saying "[... blob data]".  Use `tr`
+            # to prevent that.  See https://www.shellcheck.net/wiki/SC2312 for
+            # more info on the exec approach being taken.
+            # shellcheck disable=SC2312
+            exec {stderr_wrangling_fd}> >(tr '\r' '\n' >&2)
+            stderr_wrangling_pid="$!"
+            nix-index --db "$tmpdir" --nixpkgs "$nixpkgs_path" 2>&"$stderr_wrangling_fd"
+            exec {stderr_wrangling_fd}>&-
+            wait "$stderr_wrangling_pid"
+
+            # The `mv` plus `rm --dir` combine to make sure the output file we
+            # expect to exist does exist, and nothing else is present.
+            mv -- "$tmpdir"/files "$NIX_INDEX_DATABASE"/files
+            rm --dir -- "$tmpdir"
+        }
+
+        if [[ ! -e "$target" ]]; then
+            # There is no cache, so we need to create one.
+            do_cache_update
+        elif [[ "$store_cache" -nt "$target" ]]; then
+            # The nix-index cache hasn't been updated since we updated the path
+            # to the store cache, so it must be out of date.  Presumably a
+            # previous run failed.
+            do_cache_update
         else
-            curl_time_args=()
-        fi
+            populate_nixpkgs_path
+            if [[ -e "$store_cache" ]]; then
+                cached_nixpkgs_path="$(<"$store_cache")"
+            else
+                cached_nixpkgs_path=""
+            fi
 
-        ${pkgs.curl}/bin/curl \
-            --fail \
-            --location \
-            --output "$tmpfile" \
-            --retry 12 \
-            --no-clobber \
-            --no-progress-meter \
-            "''${curl_time_args[@]}" \
-            https://github.com/me-and/nix-index-database/releases/latest/download/index-"$arch"-linux
-
-        # Won't have downloaded anything if the existing file is newer than
-        # the remote one
-        if [[ -e "$tmpfile" ]]; then
-            ${pkgs.coreutils}/bin/mv "$tmpfile" "$target"
+            if [[ "$nixpkgs_path" != "$cached_nixpkgs_path" ]]; then
+                # The current nix-index cache was generated for a different
+                # nixpkgs, so we need to update it.
+                do_cache_update
+            fi
         fi
       '';
     };
