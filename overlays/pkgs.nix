04e2da17d1093c5b6ff7e5daa9c91b402e963c67
Adam Dinwoodie <adam@dinwoodie.org>
2026-03-16 12:27:30 +0000

    overlays/pkgs: add todo re removing reliance
    I want someone to be able to use, say, the nixosModules I'm defining
    without relying on my overlays.  That includes the overlay that is
    adding my packages.  So perhaps the way to access my packages shouldn't
    be through `pkgs.mypkgs` but a more careful import or similar.  I'd
    previously wanted to avoid boilerplate imports, but I'm not sure a
    one-line boilerplate import is worse than a boilerplate `mypkgs = import
    ../pkgs` or similar.

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -1,3 +1,7 @@
+# TODO Maybe I shouldn't be relying on an overlay here.  If I want someone else
+# to be able to use one of my modules, including ones that rely on my packages,
+# they shouldn't need to also apply my overlays; that should be a separate
+# option.
 { inputs }:
 final: prev: {
   mylib = import ../lib { inherit (final) lib; };
