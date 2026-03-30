7c00e71bff9f6e1ec8daace5d00b260679b6ddec
Adam Dinwoodie <adam@dinwoodie.org>
2026-03-30 14:17:06 +0100

    NixOS/nix-serve: add warning about secret location

--- nixos/default/default.nix
+++ nixos/default/default.nix
@@ -86,6 +86,6 @@ in
     openFirewall = true;
     # TODO This is a stupid place for this file and it should be somewhere
     # better.  Like sops-nix.
-    secretKeyFile = "/home/adam/store-secret";
+    secretKeyFile = lib.warn "need to move nix-serve secret" "/home/adam/store-secret";
   };
 }
