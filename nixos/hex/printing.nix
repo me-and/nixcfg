3b0c20761e52478078e7ba0e4d81de348f033d4b
Adam Dinwoodie <adam@dinwoodie.org>
2026-04-04 21:55:00 +0100

    NixOS/hex: permit some pypdf3 vulnerabilities

--- nixos/hex/printing.nix
+++ nixos/hex/printing.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ mylib, pkgs, ... }:
 {
   services.printing.enable = true;
   services.printing.drivers = with pkgs; [
@@ -11,4 +11,24 @@
     "cups-kyocera-3500-4500"
     "cnijfilter2"
   ];
+
+  # TODO replace pypdf3 in cups-kyocera-3500-4500 with pypdf3.  See
+  # https://github.com/NixOS/nixpkgs/pull/506381 for similar work for a
+  # different Kyocera driver set.
+  #
+  # In the meantime, I've reviewed these vulnerabilities and, while they're far
+  # from ideal, I'm not desperately worried about them.
+  nixpkgs.config.allowInsecurePredicate =
+    pkg:
+    pkg.pname == "pypdf3"
+    &&
+      (mylib.removeAll [
+        "CVE-2026-27024"
+        "CVE-2026-27025"
+        "CVE-2026-27628"
+        "CVE-2026-27888"
+        "CVE-2026-28351"
+        "CVE-2026-33699"
+      ] pkg.meta.knownVulnerabilities) == [ ];
+
 }
