cc7f56163edae420b5c2261a6abf5207421696a2
Adam Dinwoodie <adam@dinwoodie.org>
2026-04-19 10:39:20 +0100

    NixOS/Hex: add Kyocera printer config

--- nixos/hex/printing.nix
+++ nixos/hex/printing.nix
@@ -1,4 +1,4 @@
-{ mylib, pkgs, ... }:
+{ pkgs, ... }:
 {
   services.printing.enable = true;
   services.printing.drivers = with pkgs; [
@@ -11,4 +11,20 @@
     "cnijfilter2"
   ];
 
+  hardware.printers = {
+    ensureDefaultPrinter = "Kyocera";
+    ensurePrinters = [
+      {
+        name = "Kyocera";
+        description = "Kyocera ECOSYS MA3500cix";
+        model = "Kyocera/Kyocera_ECOSYS_MA3500cix.ppd";
+        deviceUri = "dnssd://Kyocera%20ECOSYS%20MA3500cix._ipps._tcp.local/?uuid=4509a320-0031-0074-011d-00f91f8d23d9";
+        ppdOptions = {
+          PageSize = "A4";
+          Duplex = "DuplexNoTumble";
+          MediaType = "Plain";
+        };
+      }
+    ];
+  };
 }
