46410c6931329a8b5eb222fc7b94a86e5309be8e
Adam Dinwoodie <adam@dinwoodie.org>
2026-05-18 21:21:39 +0100

    NixOS/Hex: add Bluetooth patch

--- nixos/hex/configuration.nix
+++ nixos/hex/configuration.nix
@@ -1,5 +1,6 @@
 {
   config,
+  pkgs,
   nixos-hardware,
   personalCfg,
   ...
@@ -62,4 +63,14 @@
   };
 
   services.nix-serve.enable = true;
+
+  boot.kernelPatches = [
+    {
+      name = "Bluetooth: btmtk: accept too short WMT FUNC_CTRL events";
+      patch = pkgs.fetchurl {
+        url = "https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git/patch/?id=162b1adeb057d28ad84fd8a03f3c50cf08db5c62";
+        hash = "sha256-ij0hQmC0U++AdXWQy6nycnDe6z4yaMoQIrSiLal5DHc=";
+      };
+    }
+  ];
 }
