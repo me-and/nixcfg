2e375693a65c48c200b7e219e0217bbe13d930c6
Adam Dinwoodie <adam@dinwoodie.org>
2026-05-18 11:01:24 +0100

    nix-run-available-checks: add --github
    Add a `--github` argument to `nix-run-available-checks`.  When
    specified, tests that are marked with `meta.buildOnGitHub = false` will
    be skipped as unavailable.

--- pkgs/nix-run-available-checks/filter.jq
+++ pkgs/nix-run-available-checks/filter.jq
@@ -5,5 +5,8 @@ end
 | if $missing_features != []
   then "skipping .#checks.\($system).\(.attr) as missing features \($missing_features | join(", "))\n" | stderr | empty
   end
+| if ($github != "") and (.meta.buildOnGitHub == false)
+  then "skipping .#checks.\($system).\(.attr) as buildOnGitHub is false\n" | stderr | empty
+  end
 | ("will build .#checks.\($system).\(.attr)\n" | stderr | empty),
   .drvPath
--- pkgs/nix-run-available-checks/package.nix
+++ pkgs/nix-run-available-checks/package.nix
@@ -19,9 +19,14 @@ writeCheckedShellApplication {
     system=${lib.escapeShellArg stdenv.hostPlatform.system}
     features_str="$(nix config show system-features)"
 
+    github=
     extra_args=()
     while (( $# > 0 )); do
         case "$1" in
+        -g|--github)
+            github=Yes
+            shift
+            ;;
         -k|--keep-going)
             extra_args+=(--keep-going)
             shift
@@ -34,18 +39,26 @@ writeCheckedShellApplication {
             extra_args+=(--add-root "''${1#--add-root=}")
             shift
             ;;
+        -[gk]*)
+            set -- "-''${1: 1:1}" "-''${1: 2}" "''${@: 2}"
+            ;;
         *)  printf 'unexpected argument: %q\n' "$1" >&2
             exit 64
             ;;
         esac
     done
 
-    nix-eval-jobs --flake --check-cache-status .#checks."$system" |
-        jq --from-file ${./filter.jq} \
-            --raw-output0 \
-            --arg features_str "$features_str" \
-            --arg system "$system" |
-        mapfile -d "" -t drvs_to_realise
+    nix-eval-jobs \
+        --flake \
+        --check-cache-status \
+        --meta \
+        .#checks."$system" |
+      jq --from-file ${./filter.jq} \
+        --raw-output0 \
+        --arg features_str "$features_str" \
+        --arg system "$system" \
+        --arg github "$github" |
+      mapfile -d "" -t drvs_to_realise
 
     if command -v nom >/dev/null; then
         nix-store --realise "''${extra_args[@]}" --log-format internal-json -v "''${drvs_to_realise[@]}" |& nom --json
