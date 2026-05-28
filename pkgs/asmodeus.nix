5e71dcf9964dd882f2f258659f86ba7f89843b3e
Adam Dinwoodie <adam@dinwoodie.org>
2026-05-28 11:18:48 +0100

    asmodeus: 0-unstable-2026-03-02 -> 0-unstable-2026-05-28

--- pkgs/asmodeus.nix
+++ pkgs/asmodeus.nix
@@ -8,13 +8,13 @@ let
 in
 buildPythonPackage {
   name = "asmodeus";
-  version = "0-unstable-2026-03-02";
+  version = "0-unstable-2026-05-28";
 
   src = fetchFromGitHub {
     owner = "me-and";
     repo = "asmodeus";
-    rev = "38ee7142ee08c05ad98e9c297f9e185a5127eea0";
-    hash = "sha256-ZOcVtxzZ1wg/kcoVS961r28HQzEJbswBHClShQiCOd8=";
+    rev = "ce6e2b16f850c3f754ff7c89ee61e07c26619caa";
+    hash = "sha256-wztPl0qs9eCQ/+wIQM93DbmTxIbapUAXRoBw7E4Mvg0=";
   };
 
   pyproject = true;
