3367ed27cfa35b118108355c54a568244456df6e
Adam Dinwoodie <adam@dinwoodie.org>
2026-05-31 16:13:34 +0100

    NixOS/marvin: get builds working with placeholder flake

--- nixos/marvin/servepdnixiso.nix
+++ nixos/marvin/servepdnixiso.nix
@@ -1,17 +1,27 @@
-{ config, pdnix, ... }:
+{
+  config,
+  lib,
+  pdnix,
+  ...
+}:
 let
   fqdn = config.networking.fqdn;
 
   isoPackage = pdnix.packages.x86_64-linux.default;
 in
-{
-  services.nginx = {
-    enable = true;
-    virtualHosts."${fqdn}".locations."= /${isoPackage.isoName}" = {
-      root = "${isoPackage}/iso";
-      extraConfig = ''
-        auth_basic off;
-      '';
+# Avoid build errors where the ISO image isn't available because I'm using a
+# placeholder flake.
+if pdnix ? packages then
+  {
+    services.nginx = {
+      enable = true;
+      virtualHosts."${fqdn}".locations."= /${isoPackage.isoName}" = {
+        root = "${isoPackage}/iso";
+        extraConfig = ''
+          auth_basic off;
+        '';
+      };
     };
-  };
-}
+  }
+else
+  lib.warn "not serving the PD ISO file thanks to the placeholder flake" { }
