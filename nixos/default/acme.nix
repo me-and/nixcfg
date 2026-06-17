c0835db59df469e5193df94225ff77a4db287928
Adam Dinwoodie <adam@dinwoodie.org>
2026-06-17 10:47:57 +0100

    NixOS/ACME: use Mythic Beasts DNS servers
    My skim of the documentation implies that the ACME / lego tooling should
    automatically use the authoritative DNS servers for checking DNS
    challenges.  Nonetheless, checking the DNS challenge propagation seems
    to reliably time out when I let it sort its DNS servers out itself, but
    work just fine when configured to explicitly use the Mythic Beasts
    servers.

--- nixos/default/acme.nix
+++ nixos/default/acme.nix
@@ -5,6 +5,15 @@ lib.mkIf (config.security.acme.certs != { }) {
     acceptTerms = true;
     defaults = {
       dnsProvider = "mythicbeasts";
+      # Use Mythic Beasts' own DNS servers.  This seems to make certificate
+      # renewal much more reliable.
+      # https://www.mythic-beasts.com/support/domains/nameservers
+      dnsResolver = builtins.concatStringsSep "," [
+        "45.33.127.156:53"
+        "93.93.128.67:53"
+        "[2600:3c00:e000:19::1]:53"
+        "[2a00:1098:0:80:1000::10]:53"
+      ];
       environmentFile = config.sops.secrets.mythic-beasts.path;
     };
   };
