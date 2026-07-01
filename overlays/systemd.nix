bc10c7b2a0f8419defb55a9eed7ea1fdec8cc8f8
Adam Dinwoodie <adam@dinwoodie.org>
2026-07-01 15:11:58 +0100

    systemd: disable passthru.tests in overlay

--- overlays/systemd.nix
+++ overlays/systemd.nix
@@ -8,5 +8,11 @@ final: prev: {
         hash = "sha256-jWbTXqrKiX+9KGcAjiwGNSYzNtppN6eHO7ybzFrHuOI=";
       })
     ];
+
+    # Disable the nixpkgs passthru tests: these will otherwise be run as part
+    # of the flake checks, and it seems they interact badly with having
+    # overridden the systemd package.  Rather than try to resolve that for a
+    # (hopefully) temporary local patch, just ignore the tests entirely.
+    passthru = removeAttrs prevAttrs.passthru [ "tests" ];
   });
 }
