5e0fda3aae9b9e5ca81eec1f21ad5a3ad8462f38
Adam Dinwoodie <adam@dinwoodie.org>
2026-07-06 09:52:02 +0100

    Home/Syncthing: configure defaults/ignores
    Partly so the setting is in place for any folders created using the
    Syncthing GUI, and partly so other configuration (in particular the
    rclone backups) can reference the list of ignores more easily.

--- home-manager/default/syncthing.nix
+++ home-manager/default/syncthing.nix
@@ -1,57 +1,12 @@
 { config, lib, ... }:
 let
   cfg = config.services.syncthing;
-
-  defaultIgnores = [
-    # Directory metadata
-    "(?d).DS_Store" # macOS
-    "(?d)desktop.ini" # Windows
-    "(?d)Thumbs.db" # Windows
-    "(?d).thumbnails" # Something on Android?
-    "(?d).directory" # Something on Linux?
-
-    # Links that Windows keeps creating and are system-specific.
-    "*.lnk"
-
-    # Generic temporary files.
-    "*.tmp"
-    ".Temp"
-
-    # Temporary editor files.
-    "~$*"
-    ".~lock.*#"
-    ".*.swp"
-
-    # Non-Syncthing file transfers.
-    ".rsync-partial" # rsync
-    ".rsync-tmp" # rsync
-    ".unison.*" # unison
-    "*.download" # web browsers (possibly Edge?)
-    "*.crdownload" # Google Chrome / Chromium
-    ".partial-*" # Unsure, maybe Firefox
-    "*.partial" # rclone
-
-    # Deleted files.
-    ".Trash-*" # Dolphin and possibly other Linux utils
-    ".trashed-*" # Something on Android?
-    ".caltrash" # Calibre
-
-    # Syncthing metadata.  Normally Syncthing ignores these itself, but when
-    # I'm syncing, say /home/adam and /home/adam/Documents, Syncthing will
-    # ignore the files in the roots of each sync'd directory, but try to sinc
-    # /home/adam/Documents/.st* files in /home/adam/Documents in the
-    # /home/adam share.
-    ".stversions"
-    ".stfolder"
-    ".stignore"
-    ".syncthing.*.tmp"
-  ];
 in
 {
   options.services.syncthing.settings.folders = lib.mkOption {
     type = lib.types.attrsOf (
       lib.types.submodule {
-        config.ignorePatterns = lib.mkDefault defaultIgnores;
+        config.ignorePatterns = lib.mkDefault cfg.settings."defaults/ignores".lines;
       }
     );
   };
@@ -66,6 +21,53 @@ in
         username = "adam";
         passwordFile = config.sops.secrets.syncthing.path;
       };
+
+      # Set default ignore patterns to cover anything that's created
+      # imperatively.
+      settings."defaults/ignores".lines = [
+        # Directory metadata
+        "(?d).DS_Store" # macOS
+        "(?d)desktop.ini" # Windows
+        "(?d)Thumbs.db" # Windows
+        "(?d).thumbnails" # Something on Android?
+        "(?d).directory" # Something on Linux?
+
+        # Links that Windows keeps creating and are system-specific.
+        "*.lnk"
+
+        # Generic temporary files.
+        "*.tmp"
+        ".Temp"
+
+        # Temporary editor files.
+        "~$*"
+        ".~lock.*#"
+        ".*.swp"
+
+        # Non-Syncthing file transfers.
+        ".rsync-partial" # rsync
+        ".rsync-tmp" # rsync
+        ".unison.*" # unison
+        "*.download" # web browsers (possibly Edge?)
+        "*.crdownload" # Google Chrome / Chromium
+        ".partial-*" # Unsure, maybe Firefox
+        "*.partial" # rclone
+
+        # Deleted files.
+        ".Trash-*" # Dolphin and possibly other Linux utils
+        ".trashed-*" # Something on Android?
+        ".caltrash" # Calibre
+
+        # Syncthing metadata.  Normally Syncthing ignores these itself, but when
+        # I'm syncing, say /home/adam and /home/adam/Documents, Syncthing will
+        # ignore the files in the roots of each sync'd directory, but try to sinc
+        # /home/adam/Documents/.st* files in /home/adam/Documents in the
+        # /home/adam share.
+        ".stversions"
+        ".stfolder"
+        ".stignore"
+        ".syncthing.*.tmp"
+      ];
 
       settings.folders.Public = {
         path = config.xdg.userDirs.publicShare;
