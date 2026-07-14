04de49dd4ec5048f661d4caccb8dcfa5345ac5f0
Adam Dinwoodie <adam@dinwoodie.org>
2026-07-14 14:30:32 +0100

    NixOS/mosh: extend timeout to 12 hours

--- nixos/default/mosh.nix
+++ nixos/default/mosh.nix
@@ -31,7 +31,7 @@ in
 
         # https://github.com/NixOS/nixpkgs/pull/515070
         environment.sessionVariables = lib.mkIf cfg.enable {
-          MOSH_SERVER_NETWORK_TMOUT = 60 * 60;
+          MOSH_SERVER_NETWORK_TMOUT = 60 * 60 * 12;
           MOSH_SERVER_SIGNAL_TMOUT = 60;
         };
       }
