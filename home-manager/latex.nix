9be0f5ff04b70008e359f80ef26845b72da6c7e7
Adam Dinwoodie <adam@dinwoodie.org>
2026-07-14 14:47:18 +0100

    mylatex: make a package
    This makes it easier to reference a single package with the all the
    LaTeX packages I care about.

--- home-manager/latex.nix
+++ home-manager/latex.nix
@@ -2,23 +2,4 @@
 # https://nixos.org/manual/nixpkgs/stable/#sec-language-texlive-lualatex-font-cache
 # -- this should probably update if and when the font list changes, if it's
 # possible to check and update that in a Home Manager update script.
-{ pkgs, ... }:
-{
-  home.packages = [
-    (pkgs.texliveMinimal.withPackages (
-      ps: with ps; [
-        babel-english
-        collection-basic
-        collection-latex
-        collection-latexrecommended
-        dejavu
-        hyphen-english
-        lastpage
-        latexmk
-        nowidow
-        nth
-        xurl
-      ]
-    ))
-  ];
-}
+{ pkgs, ... }: { home.packages = [ pkgs.mypkgs.mylatex ]; }
