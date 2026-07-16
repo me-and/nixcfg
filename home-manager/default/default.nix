d23ab9044345e7ef12f0287204d0c4f445a00a3b
Adam Dinwoodie <adam@dinwoodie.org>
2026-07-16 15:05:51 +0100

    cmdline: remove
    I forgot I'd already written and committed this when I created
    proc-cmdline.  That's a slightly more well-featured version, though, so
    keep that.

--- home-manager/default/default.nix
+++ home-manager/default/default.nix
@@ -43,7 +43,6 @@
       ]
       ++ (with pkgs.mypkgs; [
         aaisp-quota
-        cmdline
         coldiff
         dirup
         file-age
--- pkgs/cmdline.nix
+++
@@ -1,9 +0,0 @@
-{ writeCheckedShellApplication }:
-writeCheckedShellApplication {
-  name = "cmdline";
-  text = ''
-    pid="$1"
-    mapfile -d "" -t </proc/"$pid"/cmdline
-    printf '%q\n' "''${MAPFILE[@]}"
-  '';
-}
