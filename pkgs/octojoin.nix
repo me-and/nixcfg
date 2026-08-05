4db7d0a0a4682453374b4f19c710b5916877c862
Adam Dinwoodie <adam@dinwoodie.org>
2026-08-05 20:50:36 +0100

    octojoin: init at 1.6.0

---
+++ pkgs/octojoin.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+buildGoModule (finalAttrs: {
+  pname = "octojoin";
+  version = "1.6.0";
+
+  src = fetchFromGitHub {
+    owner = "matthewgall";
+    repo = "octojoin";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Jb4AbUnjoLWw8a6NzmCz5evLnB42yso5dziWNfGnoYA=";
+  };
+
+  vendorHash = "sha256-g+yaVIx4jxpAQ/+WrGKxhVeliYx7nLQe/zsGpxV4Fn4=";
+
+  # Tests require a writable home directory.
+  preCheck = ''
+    HOME="$(mktemp --directory --tmpdir)"
+    export HOME
+  '';
+
+  meta = {
+    description = "Utility for monitoring Octopus Energy UK accounts";
+    longDescription = ''
+      A comprehensive Go application that monitors Octopus Energy (UK) saving
+      sessions and free electricity periods, with automatic session joining and
+      a real-time web dashboard.
+    '';
+    homepage = "https://github.com/matthewgall/octojoin";
+    donationPage = "https://github.com/sponsors/matthewgall";
+    license = lib.licenses.asl20;
+    mainProgram = "octojoin";
+  };
+})
