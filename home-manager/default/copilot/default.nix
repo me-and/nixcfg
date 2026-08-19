5f2cdc1d3c6d5c2ec3ef52cdd3ad8b0622250a1c
Adam Dinwoodie <adam@dinwoodie.org>
2026-08-19 12:36:37 +0100

    Home/GitHub Copilot: don't set SSL_CERT_DIR
    Works around numtide/llm-agents.nix#8175

--- home-manager/default/copilot/default.nix
+++ home-manager/default/copilot/default.nix
@@ -7,7 +7,52 @@
 {
   programs.github-copilot-cli = {
     enable = true;
-    package = llm-agents.packages."${pkgs.stdenv.hostPlatform.system}".copilot-cli;
+    package =
+      # https://github.com/numtide/llm-agents.nix/issues/8175
+      # https://github.com/numtide/llm-agents.nix/pull/8174
+      llm-agents.packages."${pkgs.stdenv.hostPlatform.system}".copilot-cli.overrideAttrs (prevAttrs: {
+        preInstall = ''
+          makeWrapper_type="$(type -t makeWrapper)"
+
+          if [[ "$makeWrapper_type" != function ]]; then
+              nixErrorLog "Unexpected type of makeWrapper"
+              nixErrorLog "Expected function"
+              nixErrorLog "Got $makeWrapper_type"
+              exit 1
+          fi
+
+          makeWrapper_defn="$(declare -fp makeWrapper)"
+
+          # Wrap the makeWrapper function with a version that will suppress any
+          # `--set SSL_CERT_DIR <val>` arguments.
+          eval "''${makeWrapper_defn/#makeWrapper /makeWrapperInner }"
+          makeWrapper () {
+              local -a args=()
+              local seen_ssl_cert_dir=
+              while (( $# > 0 )); do
+                  if (( $# >= 3 )); then
+                      if [[ "$1" = --set && "$2" = SSL_CERT_DIR ]]; then
+                          shift 3
+                      else
+                          args+=("$1")
+                          shift
+                      fi
+                  else
+                      args+=("$@")
+                      break
+                  fi
+              done
+
+              makeWrapperInner "''${args[@]}"
+          }
+        ''
+        + prevAttrs.preInstall or "";
+
+        postInstall = prevAttrs.postInstall or "" + ''
+          # Reset the definition of makeWrapper
+          eval "$makeWrapper_defn"
+        '';
+      });
 
     lspServers.nix = {
       command = lib.getExe pkgs.nil;
