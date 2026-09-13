e1757748055d7f36b90bf8a58b6ed357eae4962a
Adam Dinwoodie <adam@dinwoodie.org>
2026-09-13 16:34:17 +0100

    a5toa4: improve argument handling
    - Add a `-h`/`--help` argument.
    - Handle unexpected option arguments rather than passing them to pdfjam.
    - Improve option error handling.

--- pkgs/a5toa4.nix
+++ pkgs/a5toa4.nix
@@ -7,8 +7,35 @@ writeCheckedShellApplication {
     input=
     output=
 
+    help () {
+      echo 'a5toa4 [options] [--] <input-file>'
+      echo
+      echo 'Impose an A5 PDF document to be printed twice on a single sheet of A4.'
+      echo
+      echo 'Options:'
+      echo
+      echo '  -l|--landscape:'
+      echo '    Impose a portrait A5 document onto a landscape A4 document, rather than'
+      echo '    the default of imposing a landscape A5 document onto a portrait A4'
+      echo '    document.'
+      echo '  -o|--output <output-file>'
+      echo '    Specify the output filename (default `<input>-2up.pdf`)'
+      echo '  -h|--help'
+      echo '    Output this help message'
+    }
+
+    fail_usage () {
+      printf "%s\n" "$1" >&2
+      echo 'specify `--help` for usage instructions' >&2
+      exit 64 # EX_USAGE
+    }
+
     while (( $# > 0 )); do
       case "$1" in
+        -h|--help)
+          help
+          exit 0
+          ;;
         -l|--landscape)
           landscape=YesPlease
           shift
@@ -17,7 +44,7 @@ writeCheckedShellApplication {
           output="$2"
           shift 2
           ;;
-        -l*)
+        -[hl]*)
           set -- "-''${1: 1:1}" "-''${1: 2}" "''${@: 2}"
           ;;
         -o*)
@@ -28,16 +55,22 @@ writeCheckedShellApplication {
           ;;
         --)
           if [[ "$input" ]]; then
-            exit 64 # EX_USAGE
+            fail_usage "multiple input files specified: $input and $2"
           elif (( $# != 2 )); then
-            exit 64 # EX_USAGE
+            fail_usage "multiple input files specified: $2 and $3"
           fi
           input="$2"
           shift 2
           ;;
+        --*)
+          fail_usage "unexpected option: ''${1%%=*}"
+          ;;
+        -*)
+          fail_usage "unexpected option: ''${1:0:2}"
+          ;;
         *)
           if [[ "$input" ]]; then
-            exit 64 # EX_USAGE
+            fail_usage "multiple input files specified: $input and $1"
           fi
           input="$1"
           shift
@@ -46,7 +79,11 @@ writeCheckedShellApplication {
     done
 
     if [[ -z "$output" ]]; then
-      output="''${input%.pdf}-2up.pdf"
+      if [[ "$input" = /dev/stdin ]]; then
+        output=2up.pdf
+      else
+        output="''${input%.pdf}-2up.pdf"
+      fi
     fi
 
     if [[ "$landscape" ]]; then
