08af68cd26dd03cf77620aa64143c9a3f1c9ec85
Adam Dinwoodie <adam@dinwoodie.org>
2026-09-16 18:41:10 +0100

    Home/Miki: add some fonts

---
+++ home-manager/miki/fonts.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+{
+  fonts.fontconfig.enable = true;
+
+  home.packages = with pkgs; [
+    privatepkgs.fonts.albertus
+    atkinson-hyperlegible-next
+    cardo # Free digitisation of Bembo, aka Aldine 401
+  ];
+
+  nixpkgs.config.allowUnfreePackages = [ "albertus" ];
+}
